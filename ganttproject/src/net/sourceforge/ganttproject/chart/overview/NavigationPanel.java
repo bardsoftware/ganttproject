2e60294fdf5edc16499e9cf8f5e951e43924b723
Dmitry Barashev <Dmitry Barashev@localhost>
2011-01-01 21:48:27 +0300

    refactoring: extracted common code to ToolbarBuilder

--- ganttproject/src/net/sourceforge/ganttproject/chart/overview/NavigationPanel.java
+++ ganttproject/src/net/sourceforge/ganttproject/chart/overview/NavigationPanel.java
@@ -1,17 +1,30 @@
+/*
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2010 Dmitry Barashev
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package net.sourceforge.ganttproject.chart.overview;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
+import java.text.MessageFormat;
 import java.util.Date;
 
-import javax.swing.AbstractAction;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JToolBar;
-
 import net.sourceforge.ganttproject.IGanttProject;
+import net.sourceforge.ganttproject.action.GPAction;
 import net.sourceforge.ganttproject.chart.TimelineChart;
 import net.sourceforge.ganttproject.gui.UIFacade;
 import net.sourceforge.ganttproject.task.TaskLength;
@@ -26,53 +39,44 @@ public class NavigationPanel {
     }
 
     public Component getComponent() {
-        final JToolBar buttonBar = new JToolBar();
-        buttonBar.setFloatable(false);
-        buttonBar.setBackground(new Color(0.93f, 0.93f, 0.93f));
-        buttonBar.setBorderPainted(false);
-
-        final JButton projectStart = new JButton("<html><b>&nbsp;Start&nbsp;</b></html>");
-        projectStart.setMargin(new Insets(0, 0, 0, 0));
-        projectStart.setBorderPainted(false);
-        buttonBar.add(projectStart);
-        projectStart.addMouseListener(new HighlightOnMouseOver(projectStart, new AbstractAction() {
-            public void actionPerformed(ActionEvent arg0) {
+        class ScrollToProjectStart extends GPAction {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 myChart.setStartDate(myProject.getTaskManager().getProjectStart());
                 myChart.scrollBy(createTimeInterval(-1));
             }
-        }));
-
-        buttonBar.add(new JLabel(" | "));
-
-        final JButton today = new JButton("<html><b>&nbsp;Today&nbsp;</b></html>");
-        today.setMargin(new Insets(0, 0, 0, 0));
-        today.setBorderPainted(false);
-        buttonBar.add(today);
-        today.addMouseListener(new HighlightOnMouseOver(today, new AbstractAction() {
-            public void actionPerformed(ActionEvent arg0) {
+            @Override
+            protected String getLocalizedName() {
+                return MessageFormat.format("<html><b>&nbsp;{0}&nbsp;</b></html>", getI18n("start"));
+            }
+        }
+        class ScrollToProjectEnd extends GPAction {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Date projectEnd = myProject.getTaskManager().getProjectEnd();
+                myChart.setStartDate(projectEnd);
+                while(projectEnd.before(myChart.getEndDate())) {
+                    myChart.scrollBy(createTimeInterval(-1));
+                }
+                myChart.scrollBy(createTimeInterval(1));
+            }
+            @Override
+            protected String getLocalizedName() {
+                return MessageFormat.format("<html><b>&nbsp;{0}&nbsp;</b></html>", getI18n("end"));
+            }
+        }
+        class ScrollToToday extends GPAction {
+            @Override
+            public void actionPerformed(ActionEvent e) {
                 myChart.setStartDate(new Date());
             }
-        }));
-
-        buttonBar.add(new JLabel(" | "));
-
-        final JButton projectEnd = new JButton("<html><b>&nbsp;End&nbsp;</b></html>");
-        projectEnd.setMargin(new Insets(0, 0, 0, 0));
-        projectEnd.setBorderPainted(false);
-        buttonBar.add(projectEnd);
-        projectEnd.addMouseListener(new HighlightOnMouseOver(projectEnd, new AbstractAction() {
-            public void actionPerformed(ActionEvent arg0) {
-                final TimelineChart ganttChart = myChart;
-                final Date projectEnd = myProject.getTaskManager().getProjectEnd();
-                ganttChart.setStartDate(projectEnd);
-                while(projectEnd.before(ganttChart.getEndDate())) {
-                    ganttChart.scrollBy(createTimeInterval(-1));
-                }
-                ganttChart.scrollBy(createTimeInterval(1));
+            @Override
+            protected String getLocalizedName() {
+                return MessageFormat.format("<html><b>&nbsp;{0}&nbsp;</b></html>", "Today");
             }
-        }));
-
-        return buttonBar;
+        }
+        return new ToolbarBuilder(myChart).addButton(new ScrollToProjectStart()).addButton(new ScrollToToday())
+            .addButton(new ScrollToProjectEnd()).build();
     }
 
 	protected TaskLength createTimeInterval(int i) {
--- ganttproject/src/net/sourceforge/ganttproject/chart/overview/ZoomingPanel.java
+++ ganttproject/src/net/sourceforge/ganttproject/chart/overview/ZoomingPanel.java
@@ -1,53 +1,42 @@
+/*
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2010 Dmitry Barashev
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package net.sourceforge.ganttproject.chart.overview;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Insets;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JToolBar;
-import javax.swing.SwingConstants;
 
 import net.sourceforge.ganttproject.action.ZoomInAction;
 import net.sourceforge.ganttproject.action.ZoomOutAction;
+import net.sourceforge.ganttproject.chart.TimelineChart;
 import net.sourceforge.ganttproject.gui.UIFacade;
 
 public class ZoomingPanel {
     private final ZoomInAction myZoomInAction;
     private final ZoomOutAction myZoomOutAction;
+    private final TimelineChart myChart;
 
-    public ZoomingPanel(UIFacade workbenchFacade) {
-        myZoomInAction = new ZoomInAction(workbenchFacade.getZoomManager(), "16");
-        myZoomOutAction = new ZoomOutAction(workbenchFacade.getZoomManager(), "16");
+    public ZoomingPanel(UIFacade uiFacade, TimelineChart chart) {
+        myZoomInAction = new ZoomInAction(uiFacade.getZoomManager(), "16");
+        myZoomOutAction = new ZoomOutAction(uiFacade.getZoomManager(), "16");
+        myChart = chart;
     }
 
     public Component getComponent() {
-        JToolBar buttonBar = new JToolBar();
-        buttonBar.setBackground(new Color(0.93f, 0.93f, 0.93f));
-        buttonBar.setFloatable(false);
-        buttonBar.setBorderPainted(false);
-        final JButton zoomIn = new JButton("<html><b>&nbsp;Zoom In&nbsp;</b></html>");
-        zoomIn.setBorderPainted(false);
-        zoomIn.setMargin(new Insets(0, 0, 0, 0));
-        zoomIn.setIcon(new ImageIcon(getClass().getResource("/icons/blank_big.gif")));
-        zoomIn.setHorizontalTextPosition(SwingConstants.CENTER);
-        zoomIn.setVerticalTextPosition(SwingConstants.CENTER);
-        zoomIn.addMouseListener(new HighlightOnMouseOver(zoomIn, null));
-        zoomIn.setRolloverEnabled(true);
-        zoomIn.setBorderPainted(false);
-        zoomIn.addActionListener(myZoomInAction);
-        buttonBar.add(zoomIn);
-        buttonBar.add(new JLabel(" | "));
-        JButton zoomOut = new JButton("<html><b>&nbsp;Zoom Out&nbsp;</b></html>");
-        zoomOut.setMargin(new Insets(0, 0, 0, 0));
-        zoomOut.setRolloverEnabled(true);
-        zoomOut.setBorderPainted(false);
-        zoomOut.addActionListener(myZoomOutAction);
-        zoomOut.addMouseListener(new HighlightOnMouseOver(zoomOut, null));
-        buttonBar.add(zoomOut);
-        
-        return buttonBar;
+        return new ToolbarBuilder(myChart).addButton(myZoomInAction).addButton(myZoomOutAction).build();
     }
 }
