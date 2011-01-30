d5a18388c54b28381039a5b710db9898b57a9535
Dmitry Barashev <Dmitry Barashev@localhost>
2011-01-30 03:49:35 +0300

    LAF settings implemented as GPOption and provider

---
+++ ganttproject/src/net/sourceforge/ganttproject/gui/options/InterfaceOptionPageProvider.java
@@ -0,0 +1,46 @@
+package net.sourceforge.ganttproject.gui.options;
+
+import java.awt.Component;
+
+import net.sourceforge.ganttproject.IGanttProject;
+import net.sourceforge.ganttproject.gui.UIFacade;
+import net.sourceforge.ganttproject.gui.options.model.GPOptionGroup;
+import net.sourceforge.ganttproject.gui.options.model.OptionPageProvider;
+import net.sourceforge.ganttproject.language.GanttLanguage;
+
+public class InterfaceOptionPageProvider implements OptionPageProvider {
+
+    private IGanttProject myProject;
+    private UIFacade myUiFacade;
+
+    @Override
+    public void init(IGanttProject project, UIFacade uiFacade) {
+        myProject = project;
+        myUiFacade = uiFacade;
+    }
+
+    @Override
+    public GPOptionGroup[] getOptionGroups() {
+        return new GPOptionGroup[] {myUiFacade.getOptions()};
+    }
+
+    @Override
+    public String getPageID() {
+        return "ui";
+    }
+
+    @Override
+    public boolean hasCustomComponent() {
+        return false;
+    }
+
+    @Override
+    public Component buildPageComponent() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return GanttLanguage.getInstance().getText("looknfeel");
+    }
+}
