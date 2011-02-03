f96bec85efa6d72b80e05bfa3f841ee1ec033c6f
Dmitry Barashev <Dmitry Barashev@localhost>
2011-02-04 02:19:44 +0300

    Added roles page provider

--- ganttproject/src/net/sourceforge/ganttproject/gui/options/InterfaceOptionPageProvider.java
+++ ganttproject/src/net/sourceforge/ganttproject/gui/options/InterfaceOptionPageProvider.java
@@ -1,12 +1,6 @@
 package net.sourceforge.ganttproject.gui.options;
 
-import java.awt.Component;
-
-import net.sourceforge.ganttproject.IGanttProject;
-import net.sourceforge.ganttproject.gui.UIFacade;
 import net.sourceforge.ganttproject.gui.options.model.GPOptionGroup;
-import net.sourceforge.ganttproject.gui.options.model.OptionPageProvider;
-import net.sourceforge.ganttproject.language.GanttLanguage;
 
 public class InterfaceOptionPageProvider extends OptionPageProviderBase {
 
