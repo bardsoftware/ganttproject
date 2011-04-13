c6654999d0a7c139bca9cbaf6f76618160122781
Maarten Bezemer <Maarten Bezemer@localhost>
2011-04-13 23:01:51 +0200

    Cleaned CVS exporter a bit and added StringUtils class

--- ganttproject/src/net/sourceforge/ganttproject/export/ExporterToCSV.java
+++ ganttproject/src/net/sourceforge/ganttproject/export/ExporterToCSV.java
@@ -1,9 +1,21 @@
 /*
- * Created on 17.12.2005
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2011 Dmitry Barashev
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package net.sourceforge.ganttproject.export;
 
 import java.awt.Component;
@@ -48,10 +60,7 @@ public class ExporterToCSV implements Exporter, ExportFileWizardImpl.LegacyOptio
 	}
 
 	public void run(File outputFile, ExportFinalizationJob finalizationJob) throws Exception {
-		// TODO Auto-generated method stub
-		if (!outputFile.exists()) {
-			outputFile.createNewFile();
-		}
+		outputFile.createNewFile();
 		GanttCSVExport legacyExporter = new GanttCSVExport(myProject, myOptions.getCSVOptions());
 		legacyExporter.save(new FileOutputStream(outputFile));
         finalizationJob.run(new File[] {outputFile});
@@ -72,5 +81,4 @@ public class ExporterToCSV implements Exporter, ExportFileWizardImpl.LegacyOptio
 	public void setOptions(GanttOptions options) {
 		myOptions = options;
 	}
-
 }
