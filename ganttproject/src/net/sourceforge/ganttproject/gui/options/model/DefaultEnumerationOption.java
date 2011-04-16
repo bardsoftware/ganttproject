fc0785d17613e3d92da5f04ce1a1e66056833eb6
Maarten Bezemer <Maarten Bezemer@localhost>
2011-04-16 11:41:10 +0200

    Added licence and performed some minor clean up

--- ganttproject/src/net/sourceforge/ganttproject/gui/options/model/DefaultEnumerationOption.java
+++ ganttproject/src/net/sourceforge/ganttproject/gui/options/model/DefaultEnumerationOption.java
@@ -1,6 +1,21 @@
 /*
- * Created on 18.06.2005
- */
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2011 Dmitry Barashev
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package net.sourceforge.ganttproject.gui.options.model;
 
 import java.util.ArrayList;
@@ -39,16 +54,19 @@ implements EnumerationOption, ChangeValueDispatcher {
             myStringValue_ObjectValue.put(myValues[i], values[i]);
         }
     }
+
     protected String objectToString(T obj) {
         assert obj!=null;
         return obj.toString();
     }
+
     protected T stringToObject(String value) {
         if (myStringValue_ObjectValue.isEmpty()) {
             return null;
         }
         return myStringValue_ObjectValue.get(value);
     }
+
     public String[] getAvailableValues() {
         return myValues;
     }
