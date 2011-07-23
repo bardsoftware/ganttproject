6a49c5136985366280a51c91b4cbeb8ba3e19f7a
Dmitry Barashev <Dmitry Barashev@localhost>
2011-07-23 04:29:46 +0400

    no "uncommited value" anymore

--- ganttproject/src/net/sourceforge/ganttproject/gui/options/model/DefaultStringOption.java
+++ ganttproject/src/net/sourceforge/ganttproject/gui/options/model/DefaultStringOption.java
@@ -1,34 +1,8 @@
 package net.sourceforge.ganttproject.gui.options.model;
 
 public class DefaultStringOption extends GPAbstractOption<String> implements StringOption {
-
-    private String myLockedValue;
-
-    // TODO GPAbstractOption also contains a myValue, are those the same?? (If so they should be merged and made protected)
-    private String myValue;
-
     public DefaultStringOption(String id) {
         super(id);
-    }
-
-    public void setValue(String value) {
-        ChangeValueEvent event = new ChangeValueEvent(getID(), myLockedValue,
-                value);
-        myLockedValue = value;
-        fireChangeValueEvent(event);
-    }
-
-    public String getValue() {
-        return myValue;
-    }
-
-    public String getUncommitedValue() {
-        return myLockedValue;
-    }
-
-    public void commit() {
-        super.commit();
-        myValue = myLockedValue;
     }
 
     public String getPersistentValue() {
