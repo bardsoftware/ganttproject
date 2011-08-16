52e7736174febf06177bcc25628019c80e7dc407
Maarten Bezemer <Maarten Bezemer@localhost>
2011-08-16 23:44:56 +0200

    Removed unused FileFilter classes Added license/comments for remaining FileFilter classes

--- ganttproject/src/net/sourceforge/ganttproject/filter/GanttXMLFileFilter.java
+++ ganttproject/src/net/sourceforge/ganttproject/filter/GanttXMLFileFilter.java
@@ -1,24 +1,26 @@
-/***************************************************************************
- GanttXMLFileFilter.java  -  description
- -------------------
- begin                : feb 2003
- copyright            : (C) 2002 by Thomas Alexandre
- email                : alexthomas(at)ganttproject.org
- ***************************************************************************/
+/*
+GanttProject is an opensource project management tool.
+Copyright (C) 2003-2011 Thomas Alexandre, GanttProject Team
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 package net.sourceforge.ganttproject.filter;
 
 /**
- * Class to select a filter for the FileChooser objet (*.xml)
+ * Class to select a filter for the FileChooser object (*.gan and *.xml are
+ * accepted)
  */
 public class GanttXMLFileFilter extends ExtensionBasedFileFilter {
     public GanttXMLFileFilter() {
