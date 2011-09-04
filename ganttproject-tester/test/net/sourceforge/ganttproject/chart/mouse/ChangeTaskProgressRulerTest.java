b6f553019a38bf0c48b4a641172363f3be8b6764
Dmitry Barashev <Dmitry Barashev@localhost>
2011-09-05 03:08:25 +0400

    changed the way of converting mouse offset to task progress in ChangeTaskProgressInteraction. Fixes issue #275

---
+++ ganttproject-tester/test/net/sourceforge/ganttproject/chart/mouse/ChangeTaskProgressRulerTest.java
@@ -0,0 +1,117 @@
+/*
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2011 GanttProject Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package net.sourceforge.ganttproject.chart.mouse;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import junit.framework.TestCase;
+import net.sourceforge.ganttproject.TestSetupHelper;
+import net.sourceforge.ganttproject.calendar.WeekendCalendarImpl;
+import net.sourceforge.ganttproject.chart.GraphicPrimitiveContainer;
+import net.sourceforge.ganttproject.chart.GraphicPrimitiveContainer.Rectangle;
+import net.sourceforge.ganttproject.chart.TaskChartModelFacade;
+import net.sourceforge.ganttproject.task.Task;
+import net.sourceforge.ganttproject.task.TaskActivity;
+import net.sourceforge.ganttproject.task.TaskManager;
+
+import org.easymock.EasyMock;
+
+/**
+ * Tests for {@link ChangeTaskProgressRuler}.
+ *
+ * @author Dmitry Barashev
+ */
+public class ChangeTaskProgressRulerTest extends TestCase {
+
+    private static Task createTask(TaskManager taskManager) {
+        Task result = taskManager.createTask();
+        result.move(taskManager.getRootTask());
+        result.setName(String.valueOf(result.getTaskID()));
+        return result;
+    }
+
+    public void testSimpleRuler() {
+        TaskChartModelFacade mockChartModel = EasyMock.createMock(TaskChartModelFacade.class);
+        TaskManager taskManager = TestSetupHelper.newTaskManagerBuilder()
+            .withCalendar(new WeekendCalendarImpl()).build();
+        Task task = createTask(taskManager);
+        task.setStart(TestSetupHelper.newMonday());
+        task.setDuration(taskManager.createLength(2));
+
+        GraphicPrimitiveContainer primitives = new GraphicPrimitiveContainer();
+        Rectangle r = primitives.createRectangle(0, 0, 100, 10);
+
+        assertEquals(1, task.getActivities().length);
+        primitives.bind(r, task.getActivities()[0]);
+
+        EasyMock.expect(mockChartModel.getTaskRectangles(task)).andReturn(Collections.singletonList(r));
+        EasyMock.replay(mockChartModel);
+        ChangeTaskProgressRuler ruler = new ChangeTaskProgressRuler(task, mockChartModel);
+        assertEquals(0, ruler.getProgress(-1));
+        assertEquals(0, ruler.getProgress(0));
+        assertEquals(1, ruler.getProgress(1));
+        assertEquals(50, ruler.getProgress(50));
+        assertEquals(99, ruler.getProgress(99));
+        assertEquals(100, ruler.getProgress(100));
+        assertEquals(100, ruler.getProgress(101));
+        EasyMock.verify(mockChartModel);
+    }
+
+    public void testRulerOverWeekend() {
+        TaskChartModelFacade mockChartModel = EasyMock.createMock(TaskChartModelFacade.class);
+        TaskManager taskManager = TestSetupHelper.newTaskManagerBuilder()
+            .withCalendar(new WeekendCalendarImpl()).build();
+        Task task = createTask(taskManager);
+        task.setStart(TestSetupHelper.newFriday());
+        task.setDuration(taskManager.createLength(2));
+
+        TaskActivity[] activities = task.getActivities();
+        assertEquals(3, activities.length);
+
+        GraphicPrimitiveContainer primitives = new GraphicPrimitiveContainer();
+        Rectangle r0 = primitives.createRectangle(0, 0, 100, 10);
+        primitives.bind(r0, activities[0]);
+
+        Rectangle r1 = primitives.createRectangle(100, 0, 10, 10);
+        primitives.bind(r1, activities[1]);
+
+        Rectangle r2 = primitives.createRectangle(110, 0, 100, 10);
+        primitives.bind(r2, activities[2]);
+
+        EasyMock.expect(mockChartModel.getTaskRectangles(task)).andReturn(Arrays.asList(r0, r1, r2));
+        EasyMock.replay(mockChartModel);
+
+        ChangeTaskProgressRuler ruler = new ChangeTaskProgressRuler(task, mockChartModel);
+        assertEquals(0, ruler.getProgress(-1));
+        assertEquals(0, ruler.getProgress(0));
+        assertEquals(1, ruler.getProgress(2));
+        assertEquals(25, ruler.getProgress(50));
+        assertEquals(50, ruler.getProgress(100));
+        assertEquals(50, ruler.getProgress(101));
+        assertEquals(50, ruler.getProgress(109));
+        assertEquals(50, ruler.getProgress(110));
+        assertEquals(75, ruler.getProgress(160));
+        assertEquals(100, ruler.getProgress(219));
+        assertEquals(100, ruler.getProgress(211));
+        assertEquals(100, ruler.getProgress(212));
+
+        EasyMock.verify(mockChartModel);
+    }
+}
