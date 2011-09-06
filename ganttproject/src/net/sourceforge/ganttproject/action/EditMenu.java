2f8cb10e54bfbc2ff3488297bbd07e6cbc21bf16
Maarten Bezemer <Maarten Bezemer@localhost>
2011-09-06 10:54:06 +0200

    * EditMenu now really is a JMenu  (as the name suggests) * ReadAction and UnoAction do not require undoManager as a constructor parameter

--- ganttproject/src/net/sourceforge/ganttproject/action/EditMenu.java
+++ ganttproject/src/net/sourceforge/ganttproject/action/EditMenu.java
@@ -1,6 +1,6 @@
 /*
 GanttProject is an opensource project management tool. License: GPL2
-Copyright (C) 2011 Dmitry Barashev
+Copyright (C) 2011 Dmitry Barashev, GanttProject Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -18,66 +18,32 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 package net.sourceforge.ganttproject.action;
 
-import java.awt.event.ActionEvent;
-
 import javax.swing.Action;
 import javax.swing.JMenu;
 
 import net.sourceforge.ganttproject.GPViewManager;
 import net.sourceforge.ganttproject.IGanttProject;
 import net.sourceforge.ganttproject.gui.UIFacade;
-import net.sourceforge.ganttproject.undo.GPUndoManager;
 
-public class EditMenu {
-    private final IGanttProject myProject;
-    private final UIFacade myUiFacade;
-    private final GPViewManager myViewManager;
+public class EditMenu extends JMenu {
     private final UndoAction myUndoAction;
     private final RedoAction myRedoAction;
 
     public EditMenu(IGanttProject project, UIFacade uiFacade, GPViewManager viewManager) {
-        myProject = project;
-        myUiFacade = uiFacade;
-        myViewManager = viewManager;
-        myUndoAction = new UndoAction(getUndoManager(), getUIFacade());
-        myRedoAction = new RedoAction(getUndoManager(), getUIFacade());
-    }
-
-    public JMenu create() {
-        JMenu result = new JMenu(new GPAction("edit") {
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-            }
-        });
-        result.add(getUndoAction());
-        result.add(getRedoAction());
-        result.addSeparator();
-        result.add(new RefreshViewAction(getUIFacade()));
-        result.add(new SearchDialogAction(getProject(), getUIFacade()));
-        result.addSeparator();
-        result.add(getViewManager().getCutAction());
-        result.add(getViewManager().getCopyAction());
-        result.add(getViewManager().getPasteAction());
-        result.addSeparator();
-        result.add(new SettingsDialogAction(getProject(), getUIFacade()));
-
-        return result;
-    }
-
-    private UIFacade getUIFacade() {
-        return myUiFacade;
-    }
-
-    private IGanttProject getProject() {
-        return myProject;
-    }
-
-    private GPUndoManager getUndoManager() {
-        return myUiFacade.getUndoManager();
-    }
-
-    private GPViewManager getViewManager() {
-        return myViewManager;
+        myUndoAction = new UndoAction(uiFacade);
+        myRedoAction = new RedoAction(uiFacade);
+        
+        add(getUndoAction());
+        add(getRedoAction());
+        addSeparator();
+        add(new RefreshViewAction(uiFacade));
+        add(new SearchDialogAction(project,uiFacade));
+        addSeparator();
+        add(viewManager.getCutAction());
+        add(viewManager.getCopyAction());
+        add(viewManager.getPasteAction());
+        addSeparator();
+        add(new SettingsDialogAction(project, uiFacade));
     }
 
     public Action getUndoAction() {
