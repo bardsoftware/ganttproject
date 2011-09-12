9bb51a3387dcd2ec9f68f4bb3a77d0837d29be34
maarten bezemer <maarten.bezemer@gmail.com>
2011-09-12 17:50:13 +0200

    Closes issue #279: Hidden columns information does not get saved/loaded Thanks marcus11 for providing the patch
    Also added license and some quick cleanup

--- ganttproject/src/net/sourceforge/ganttproject/io/GanttChartViewSaver.java
+++ ganttproject/src/net/sourceforge/ganttproject/io/GanttChartViewSaver.java
@@ -1,3 +1,21 @@
+/*
+GanttProject is an opensource project management tool. License: GPL2
+Copyright (C) 2011 GanttProject Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package net.sourceforge.ganttproject.io;
 
 import javax.xml.transform.sax.TransformerHandler;
@@ -17,12 +35,11 @@ class GanttChartViewSaver extends SaverBase {
         final TableHeaderUIFacade tableHeader = treeTable.getVisibleFields();
         for (int i=0; i<tableHeader.getSize(); i++) {
             Column column = tableHeader.getField(i);
-            if (column.isVisible()) {
-                addAttribute("property-id", column.getID(), attrs);
-                addAttribute("order", column.getOrder(), attrs);
-                addAttribute("width", column.getWidth(), attrs);
-                emptyElement("displaycolumn", attrs, handler);
-            }
+            addAttribute("property-id", column.getID(), attrs);
+            addAttribute("order", column.getOrder(), attrs);
+            addAttribute("width", column.getWidth(), attrs);
+            addAttribute("visible", column.isVisible(), attrs);
+            emptyElement("displaycolumn", attrs, handler);
         }
         endElement("taskdisplaycolumns", handler);
     }
