418fa28e8b5f87fd1ef445f537b2c7473c5e58e5
dbarashev <dbarashev@localhost>
2012-01-23 13:15:23 +0400

    #fix do not render progress bar for milestones

--- ganttproject/src/net/sourceforge/ganttproject/chart/TaskRendererImpl2.java
+++ ganttproject/src/net/sourceforge/ganttproject/chart/TaskRendererImpl2.java
@@ -3,7 +3,7 @@ Copyright 2003-2012 Dmitry Barashev, GanttProject Team
 
 This file is part of GanttProject, an opensource project management tool.
 
-GanttProject is free software: you can redistribute it and/or modify 
+GanttProject is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
@@ -184,7 +184,7 @@ public class TaskRendererImpl2 extends ChartRendererBase {
             myModel, getPrimitiveContainer(), myLabelsRenderer,
             new TaskActivityRenderer.Style(0, getRectangleHeight()));
         List<Rectangle> rectangles = activityRenderer.renderActivities(rowNum, activities, defaultUnitOffsets);
-        if (!getChartModel().getTaskManager().getTaskHierarchy().hasNestedTasks(t)) {
+        if (!getChartModel().getTaskManager().getTaskHierarchy().hasNestedTasks(t) && !t.isMilestone()) {
             renderProgressBar(rectangles);
         }
         return rectangles;
