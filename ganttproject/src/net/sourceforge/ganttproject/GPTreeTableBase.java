e746e27e52ade359d5ed71ad2ca3513d9f5ce651
dbarashev <dbarashev@localhost>
2012-03-31 01:13:30 +0400

    fixes issue #465

--- ganttproject/src/net/sourceforge/ganttproject/GPTreeTableBase.java
+++ ganttproject/src/net/sourceforge/ganttproject/GPTreeTableBase.java
@@ -102,8 +102,14 @@ public abstract class GPTreeTableBase extends JNTreeTable implements CustomPrope
     @Override
     public void actionPerformed(ActionEvent e) {
       JTable t = getTable();
-      TreeTableCellEditorImpl cellEditor = (TreeTableCellEditorImpl) getTable().getCellEditor(t.getSelectedRow(),
-          t.getSelectedColumn());
+      if (t.getSelectedRow() < 0) {
+        return;
+      }
+      if (t.getSelectedColumn() < 0) {
+        t.getColumnModel().getSelectionModel().setSelectionInterval(0, 0);
+      }
+      TreeTableCellEditorImpl cellEditor = (TreeTableCellEditorImpl) getTable().getCellEditor(
+          t.getSelectedRow(), t.getSelectedColumn());
       t.editCellAt(t.getSelectedRow(), t.getSelectedColumn());
       cellEditor.requestFocus();
     }
