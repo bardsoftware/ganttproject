973cf39bde3b2c535cda9b29f23c076cf6255f97
dbarashev <dbarashev@localhost>
2012-06-26 15:01:34 +0400

    #refactoring extracted dialog builder into a separate class

--- ganttproject/src/net/sourceforge/ganttproject/UIFacadeImpl.java
+++ ganttproject/src/net/sourceforge/ganttproject/UIFacadeImpl.java
@@ -18,15 +18,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package net.sourceforge.ganttproject;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Frame;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Arrays;
@@ -36,33 +29,23 @@ import java.util.Date;
 import java.util.Locale;
 import java.util.logging.Level;
 
-import javax.swing.AbstractAction;
 import javax.swing.Action;
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
-import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.WindowConstants;
 import javax.swing.event.HyperlinkEvent;
 import javax.swing.event.HyperlinkEvent.EventType;
 import javax.swing.event.HyperlinkListener;
 
-import net.sourceforge.ganttproject.action.CancelAction;
-import net.sourceforge.ganttproject.action.OkAction;
 import net.sourceforge.ganttproject.action.zoom.ZoomActionSet;
 import net.sourceforge.ganttproject.chart.Chart;
 import net.sourceforge.ganttproject.chart.GanttChart;
 import net.sourceforge.ganttproject.chart.TimelineChart;
 import net.sourceforge.ganttproject.document.Document.DocumentException;
-import net.sourceforge.ganttproject.gui.DialogAligner;
 import net.sourceforge.ganttproject.gui.GanttLookAndFeelInfo;
 import net.sourceforge.ganttproject.gui.GanttLookAndFeels;
 import net.sourceforge.ganttproject.gui.GanttStatusBar;
@@ -109,10 +92,12 @@ class UIFacadeImpl extends ProgressProvider implements UIFacade {
   private final LafOption myLafOption;
   private final NotificationManagerImpl myNotificationManager;
   private final TaskView myTaskView = new TaskView();
+  private final DialogBuilder myDialogBuilder;
 
   UIFacadeImpl(JFrame mainFrame, GanttStatusBar statusBar, NotificationManagerImpl notificationManager,
       IGanttProject project, UIFacade fallbackDelegate) {
     myMainFrame = mainFrame;
+    myDialogBuilder = new DialogBuilder(mainFrame);
     myScrollingManager = new ScrollingManagerImpl();
     myZoomManager = new ZoomManager(project.getTimeUnitStack());
     myStatusBar = statusBar;
@@ -240,101 +225,7 @@ class UIFacadeImpl extends ProgressProvider implements UIFacade {
 
   @Override
   public Dialog createDialog(Component content, Action[] buttonActions, String title) {
-    final JDialog dlg = new JDialog(myMainFrame, true);
-    final Dialog result = new Dialog() {
-      @Override
-      public void hide() {
-        if (dlg.isVisible()) {
-          dlg.setVisible(false);
-          dlg.dispose();
-        }
-      }
-
-      @Override
-      public void show() {
-        center(Centering.WINDOW);
-        dlg.setVisible(true);
-      }
-
-      @Override
-      public void layout() {
-        dlg.pack();
-      }
-
-      @Override
-      public void center(Centering centering) {
-        DialogAligner.center(dlg, myMainFrame, centering);
-      }
-    };
-    dlg.setTitle(title);
-    final Commiter commiter = new Commiter();
-    Action cancelAction = null;
-    JPanel buttonBox = new JPanel(new GridLayout(1, buttonActions.length, 5, 0));
-    for (final Action nextAction : buttonActions) {
-      JButton nextButton = null;
-      if (nextAction instanceof OkAction) {
-        nextButton = new JButton(nextAction);
-        nextButton.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            result.hide();
-            commiter.commit();
-          }
-        });
-        if (((OkAction)nextAction).isDefault()) {
-          dlg.getRootPane().setDefaultButton(nextButton);
-        }
-      }
-      if (nextAction instanceof CancelAction) {
-        cancelAction = nextAction;
-        nextButton = new JButton(nextAction);
-        nextButton.addActionListener(new ActionListener() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            result.hide();
-            commiter.commit();
-          }
-        });
-        dlg.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), nextAction.getValue(Action.NAME));
-        dlg.getRootPane().getActionMap().put(nextAction.getValue(Action.NAME), new AbstractAction() {
-          @Override
-          public void actionPerformed(ActionEvent e) {
-            nextAction.actionPerformed(e);
-            result.hide();
-          }
-        });
-      }
-      if (nextButton == null) {
-        nextButton = new JButton(nextAction);
-      }
-      buttonBox.add(nextButton);
-      KeyStroke accelerator = (KeyStroke) nextAction.getValue(Action.ACCELERATOR_KEY);
-      if (accelerator != null) {
-        dlg.getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(accelerator, nextAction);
-        dlg.getRootPane().getActionMap().put(nextAction, nextAction);
-      }
-    }
-    dlg.getContentPane().setLayout(new BorderLayout());
-    dlg.getContentPane().add(content, BorderLayout.CENTER);
-
-    JPanel buttonPanel = new JPanel(new BorderLayout());
-    buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 5));
-    buttonPanel.add(buttonBox, BorderLayout.EAST);
-    dlg.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
-
-    dlg.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    final Action localCancelAction = cancelAction;
-    dlg.addWindowListener(new WindowAdapter() {
-      @Override
-      public void windowClosed(WindowEvent e) {
-        if (localCancelAction != null && !commiter.isCommited()) {
-          localCancelAction.actionPerformed(null);
-        }
-      }
-    });
-    dlg.pack();
-    return result;
+    return myDialogBuilder.createDialog(content, buttonActions, title);
   }
 
   @Override
@@ -469,18 +360,6 @@ class UIFacadeImpl extends ProgressProvider implements UIFacade {
   @Override
   public Frame getMainFrame() {
     return myMainFrame;
-  }
-
-  private static class Commiter {
-    private boolean isCommited;
-
-    void commit() {
-      isCommited = true;
-    }
-
-    boolean isCommited() {
-      return isCommited;
-    }
   }
 
   private static GanttLanguage getLanguage() {
