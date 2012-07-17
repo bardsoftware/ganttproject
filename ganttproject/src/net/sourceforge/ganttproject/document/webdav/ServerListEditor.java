e3576168f421f184dd9b8c8b8f81ee19635a69dd
dbarashev <dbarashev@localhost>
2012-07-17 20:16:40 +0400

    username is now saved along with server name and root url

--- ganttproject/src/net/sourceforge/ganttproject/document/webdav/ServerListEditor.java
+++ ganttproject/src/net/sourceforge/ganttproject/document/webdav/ServerListEditor.java
@@ -1,3 +1,21 @@
+/*
+Copyright 2012 GanttProject Team
+
+This file is part of GanttProject, an opensource project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package net.sourceforge.ganttproject.document.webdav;
 
 import java.awt.BorderLayout;
@@ -39,35 +57,46 @@ import net.sourceforge.ganttproject.gui.TestGanttRolloverButton;
 import net.sourceforge.ganttproject.gui.UIUtil;
 import net.sourceforge.ganttproject.gui.options.model.ListOption;
 
+/**
+ * Editor for WebDAV server list in WebDAV open/save dialog
+ *
+ * @author dbarashev (Dmitry Barashev)
+ */
 class ServerListEditor {
   private Action myPopupAction = new AbstractAction() {
+    @Override
     public void actionPerformed(ActionEvent e) {
       onPopupAction();
     }
   };
 
   private Action myDoneAction = new GPAction("done") {
+    @Override
     public void actionPerformed(ActionEvent arg0) {
       onDoneAction();
     }
   };
 
   private Action myPlusAction = new GPAction("add", IconSize.SMALL) {
+    @Override
     public void actionPerformed(ActionEvent arg0) {
+      myOption.addValue(new WebDavServerDescriptor("", "", ""));
       myTableModel.addRow(new String[] {"", ""});
       myTable.editCellAt(myTableModel.getRowCount() - 1, 0);
     }
   };
 
   private Action myMinusAction = new GPAction("delete", IconSize.SMALL) {
+    @Override
     public void actionPerformed(ActionEvent e) {
+      myOption.removeValueIndex(myTable.getSelectedRow());
       myTableModel.removeRow(myTable.getSelectedRow());
     }
   };
 
   private JButton myButton = new TestGanttRolloverButton(myPopupAction);
   private JPanel myWrapper = new JPanel(new FlowLayout(FlowLayout.LEADING, 0, 0));
-  private ListOption myOption;
+  private ListOption<WebDavServerDescriptor> myOption;
   private DefaultTableModel myTableModel;
   private JXTable myTable;
 
@@ -78,32 +107,51 @@ class ServerListEditor {
   private JButton myDoneButton;
 
   private ListSelectionListener mySelectionListener = new ListSelectionListener() {
+    @Override
     public void valueChanged(ListSelectionEvent arg0) {
       int selectedRow = myTable.getSelectedRow();
       if (selectedRow == -1) {
         return;
       }
-      myOption.setValue(buildValue(selectedRow));
+      myOption.setValueIndex(selectedRow);
       myButton.setText(String.valueOf(myTable.getValueAt(selectedRow, 0)));
     }
   };
 
   private JPopupMenu myPopup;
 
-  public ServerListEditor(ListOption option) {
+  public ServerListEditor(ListOption<WebDavServerDescriptor> option) {
     myOption = option;
-    myButton.setText(option.getValue().split("\\t")[0]);
+    myButton.setText(option.getValue().name);
     myButton.setIcon(new ImageIcon(getClass().getResource("/icons/dropdown_16.png")));
     myButton.setHorizontalTextPosition(SwingConstants.LEADING);
     myButton.setVerticalTextPosition(SwingConstants.CENTER);
 
     myWrapper.add(myButton);
-    myTableModel = new DefaultTableModel(new Vector<String>(Arrays.asList("Name", "URL")), 0);
-    for (String value : option.getAvailableValues()) {
-      String[] parts = value.split("\\t");
-      if (parts.length == 2) {
-        myTableModel.addRow(parts);
+
+    myTableModel = new DefaultTableModel(new Vector<String>(Arrays.asList("Name", "URL")), 0) {
+      @Override
+      public void setValueAt(Object aValue, int row, int column) {
+        super.setValueAt(aValue, row, column);
+
+        String editValue = String.valueOf(aValue);
+        List<WebDavServerDescriptor> values = Lists.newArrayList(myOption.getValues());
+        WebDavServerDescriptor descr = values.get(row);
+        switch (column) {
+        case 0:
+          descr.name = editValue;
+          break;
+        case 1:
+          descr.rootUrl = editValue;
+          break;
+        default:
+          assert false : "should not be here";
+        }
       }
+
+    };
+    for (WebDavServerDescriptor value : option.getValues()) {
+      myTableModel.addRow(new String[] {value.name, value.rootUrl});
     }
     myTable = new JXTable(myTableModel);
     myTable.setVisibleRowCount(10);
@@ -145,18 +193,19 @@ class ServerListEditor {
     myPopup = new JPopupMenu();
     myPopup.add(myResult);
     myPopup.addPopupMenuListener(new PopupMenuListener() {
+      @Override
       public void popupMenuWillBecomeVisible(PopupMenuEvent arg0) {
-        // TODO Auto-generated method stub
         TableColumn column = myTable.getColumn(0);
         Dimension dimension = UIUtil.autoFitColumnWidth(myTable, column);
-        System.err.println("autofit="+dimension);
         column.setWidth(dimension.width);
         column.setPreferredWidth(dimension.width);
       }
+      @Override
       public void popupMenuWillBecomeInvisible(PopupMenuEvent arg0) {
         applyValues();
       }
 
+      @Override
       public void popupMenuCanceled(PopupMenuEvent arg0) {
         applyValues();
       }
@@ -165,11 +214,6 @@ class ServerListEditor {
   }
 
   private void applyValues() {
-    List<String> values = Lists.newArrayList();
-    for (int i = 0; i < myTableModel.getRowCount(); i++) {
-      values.add(buildValue(i));
-    }
-    myOption.setValues(values);
     mySelectionListener.valueChanged(null);
   }
 
---
+++ ganttproject/src/net/sourceforge/ganttproject/document/webdav/WebDavServerDescriptor.java
@@ -0,0 +1,55 @@
+/*
+Copyright 2012 GanttProject Team
+
+This file is part of GanttProject, an opensource project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package net.sourceforge.ganttproject.document.webdav;
+
+import com.google.common.base.Objects;
+
+/**
+ * Encapsulates server access information.
+ *
+ * @author dbarashev (Dmitry Barashev)
+ */
+class WebDavServerDescriptor {
+  public String name;
+  public String rootUrl;
+  public String username;
+
+  WebDavServerDescriptor() {
+  }
+
+  WebDavServerDescriptor(String name, String rootUrl, String username) {
+    this.name = name;
+    this.rootUrl = rootUrl;
+    this.username = username;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof WebDavServerDescriptor == false) {
+      return false;
+    }
+    WebDavServerDescriptor that = (WebDavServerDescriptor) obj;
+    return Objects.equal(this.rootUrl, that.rootUrl);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(this.rootUrl);
+  }
+}
