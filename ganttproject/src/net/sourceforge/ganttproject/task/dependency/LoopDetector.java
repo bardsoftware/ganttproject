71b7acdc90982116dc05046f7b536c00e6d3218f
dbarashev <dbarashev@localhost>
2012-12-05 17:48:06 +0400

    use dependency graph transaction for loop detection

--- ganttproject/src/net/sourceforge/ganttproject/task/dependency/LoopDetector.java
+++ ganttproject/src/net/sourceforge/ganttproject/task/dependency/LoopDetector.java
@@ -18,16 +18,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package net.sourceforge.ganttproject.task.dependency;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
-import net.sourceforge.ganttproject.task.Task;
 import net.sourceforge.ganttproject.task.TaskManager;
+import net.sourceforge.ganttproject.task.algorithm.DependencyGraph;
 
 /**
  * Loop detector answers whether a dependency will create a loop in the
  * dependency graph
- * 
+ *
  * @author dbarashev
  */
 public class LoopDetector {
@@ -38,29 +35,18 @@ public class LoopDetector {
   }
 
   public boolean isLooping(TaskDependency dep) {
-    Set<Task> checked = new LinkedHashSet<Task>();
-    checked.add(dep.getDependee());
-    return isLooping(checked, dep.getDependant());
-  }
-
-  private boolean isLooping(Set<Task> checked, Task incoming) {
-    boolean result = false;
-    Set<Task> newChecked = new LinkedHashSet<Task>(checked);
-    newChecked.add(incoming);
-    TaskDependency[] nextDeps = incoming.getDependenciesAsDependee().toArray();
-    for (int i = 0; !result && i < nextDeps.length; i++) {
-      if (!newChecked.contains(nextDeps[i].getDependant())) {
-        result = isLooping(newChecked, nextDeps[i].getDependant());
-      } else {
-        result = true;
-      }
+    DependencyGraph graph = myTaskManager.getDependencyGraph();
+    DependencyGraph.Logger oldLogger = graph.getLogger();
+    graph.startTransaction();
+    try {
+      graph.setLogger(DependencyGraph.THROWING_LOGGER);
+      graph.addDependency(dep);
+      return false;
+    } catch (TaskDependencyException e) {
+      return true;
+    } finally {
+      graph.rollbackTransaction();
+      graph.setLogger(oldLogger);
     }
-    if (!result) {
-      Task supertask = myTaskManager.getTaskHierarchy().getContainer(incoming);
-      if (supertask != null && myTaskManager.getTaskHierarchy().getRootTask() != supertask) {
-        result = isLooping(newChecked, supertask);
-      }
-    }
-    return result;
   }
 }
