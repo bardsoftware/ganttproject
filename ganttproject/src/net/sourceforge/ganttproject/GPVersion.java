1f1c7852cb9eb16f108b0b3c12ea829ca708f75b
dbarashev <dbarashev@localhost>
2013-11-27 02:50:40 +0400

    really fixes issue #743 version 2.6.2-r1544

--- ganttproject/src/net/sourceforge/ganttproject/GPVersion.java
+++ ganttproject/src/net/sourceforge/ganttproject/GPVersion.java
@@ -33,7 +33,7 @@ public abstract class GPVersion {
   public static String PRAHA = "2.5.5 Praha (build 1256)";
   public static String BRNO = "2.6 Brno (build 1473)";
   public static String BRNO_2_6_1 = "2.6.1 Brno (build 1499)";
-  public static String BRNO_2_6_2 = "2.6.2 Brno (build 1541)";
+  public static String BRNO_2_6_2 = "2.6.2 Brno (build 1544)";
   public static String CURRENT = BRNO_2_6_2;
 
   public static String getCurrentVersionNumber() {
