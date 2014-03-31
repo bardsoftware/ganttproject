955ba6b34182c62680720f8964b86a27b421be7a
dbarashev <dbarashev@localhost>
2014-04-01 03:51:36 +0400

    refactoring: CSV import/export classes moved to biz.ganttproject.impex.csv package added: cost column to import

--- ganttproject-tester/test/biz/ganttproject/impex/csv/GPCsvImportTest.java
+++ ganttproject-tester/test/biz/ganttproject/impex/csv/GPCsvImportTest.java
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
 */
-package net.sourceforge.ganttproject.io;
+package biz.ganttproject.impex.csv;
 
 import java.io.Reader;
 import java.io.StringReader;
