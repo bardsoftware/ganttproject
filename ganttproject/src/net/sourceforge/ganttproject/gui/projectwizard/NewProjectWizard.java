fbfe6f34fe0d26108df2af5edded78124dd51ac7
dbarashev <dbarashev@localhost>
2014-04-03 19:41:07 +0400

    use buffer calendar in New Project wizard (otherwise it would modify the existing one)

--- ganttproject/src/net/sourceforge/ganttproject/gui/projectwizard/NewProjectWizard.java
+++ ganttproject/src/net/sourceforge/ganttproject/gui/projectwizard/NewProjectWizard.java
@@ -3,7 +3,7 @@ Copyright 2003-2012 Dmitry Barashev, GanttProject Team
 
 This file is part of GanttProject, an opensource project management tool.
 
-GanttProject is free software: you can redistribute it and/or modify 
+GanttProject is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
@@ -27,10 +27,10 @@ public class NewProjectWizard {
 
   public PrjInfos createNewProject(IGanttProject project, UIFacade uiFacade) {
     RoleSet[] roleSets = project.getRoleManager().getRoleSets();
-    NewProjectWizardWindow newProjectWizard = new NewProjectWizardWindow(uiFacade, new I18N());
+    NewProjectWizardWindow newProjectWizard = new NewProjectWizardWindow(project, uiFacade, new I18N());
     newProjectWizard.addProjectNamePage(project);
     newProjectWizard.addRoleSetPage(roleSets);
-    newProjectWizard.addWeekendConfigurationPage(project.getActiveCalendar(), project);
+    newProjectWizard.addWeekendConfigurationPage(project);
     newProjectWizard.show();
     return new PrjInfos();
   }
