bc2051d0c5f9f2bda8834c38062cac987c89e2af
Dmitry Barashev <dbarashev+committer@ganttproject.biz>
2017-02-16 03:47:20 +0300

    assorted fixes: Windows launcher option to disable Direct3D (update #1301 #1340) Mac OSX Info.plist to enable high-resolution capability (update #1108) ANT buildfile update (#1331)

--- ganttproject-builder/ganttproject.bat
+++ ganttproject-builder/ganttproject.bat
@@ -10,7 +10,7 @@ SET LOCAL_CLASSPATH=%GP_HOME%\eclipsito.jar;%GP_HOME%
 SET CONFIGURATION_FILE=ganttproject-eclipsito-config.xml
 SET BOOT_CLASS=org.bardsoftware.eclipsito.Boot
 
-"%JAVA_COMMAND%" -Xmx512m -classpath "%CLASSPATH%;%LOCAL_CLASSPATH%" -ea %BOOT_CLASS% "%CONFIGURATION_FILE%" -log true  %1 %2 %3 %4 %5 %6 
+"%JAVA_COMMAND%" -Xmx512m -classpath "%CLASSPATH%;%LOCAL_CLASSPATH%" -ea -Dsun.java2d.d3d=false %BOOT_CLASS% "%CONFIGURATION_FILE%" -log true  %1 %2 %3 %4 %5 %6 
 if %ERRORLEVEL% EQU 9009 goto ERROR_NO_JAVA
 goto END
 
