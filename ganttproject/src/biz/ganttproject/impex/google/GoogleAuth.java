63e6959905f2d083dbe476feb638cef55a8a8b27
Leonid <shatuln@gmail.com>
2017-03-18 12:43:15 +0300

    Added license information

--- ganttproject/src/biz/ganttproject/impex/google/GoogleAuth.java
+++ ganttproject/src/biz/ganttproject/impex/google/GoogleAuth.java
@@ -1,3 +1,19 @@
+/*
+Copyright (C) 2017 BarD Software s.r.o., Leonid Shatunov
+
+This file is part of GanttProject, an open-source project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package biz.ganttproject.impex.google;
 
 import com.google.api.client.auth.oauth2.Credential;
@@ -19,6 +35,11 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
 
+/**
+ * Methods for google authorization
+ *
+ * @author Leonid Shatunov (shatuln@gmail.com)
+ */
 public class GoogleAuth {
 
   private static final String CLIENT_ID = "299608862260-vnb8d18h7cismqil9fbhf800jlnd3u6d.apps.googleusercontent.com";
--- ganttproject/src/biz/ganttproject/impex/google/GoogleExportOptionPageProvider.java
+++ ganttproject/src/biz/ganttproject/impex/google/GoogleExportOptionPageProvider.java
@@ -1,11 +1,25 @@
+/*
+Copyright (C) 2017 BarD Software s.r.o., Leonid Shatunov
+
+This file is part of GanttProject, an open-source project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package biz.ganttproject.impex.google;
 
 import biz.ganttproject.core.option.GPOptionGroup;
 import net.sourceforge.ganttproject.GPLogger;
 import net.sourceforge.ganttproject.action.GPAction;
 import net.sourceforge.ganttproject.gui.options.OptionPageProviderBase;
-import net.sourceforge.ganttproject.gui.options.OptionsPageBuilder;
-import net.sourceforge.ganttproject.language.GanttLanguage;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
@@ -13,6 +27,11 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.logging.Level;
 
+/**
+ * Builds option page for google authorization
+ *
+ * @author Leonid Shatunov (shatuln@gmail.com)
+ */
 public class GoogleExportOptionPageProvider extends OptionPageProviderBase {
 
   private GoogleAuth myAuth = new GoogleAuth();
