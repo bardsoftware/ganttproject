2cdf19ed31a30660b6014fe588c868fcaea23964
roman <roman@torkhov.com>
2017-04-09 21:47:23 +0300

    Import from Excel. Tests.

--- ganttproject-tester/test/biz/ganttproject/impex/csv/GPSpreadsheetImportTest.java
+++ ganttproject-tester/test/biz/ganttproject/impex/csv/GPSpreadsheetImportTest.java
@@ -37,26 +37,30 @@ import net.sourceforge.ganttproject.task.TaskContainmentHierarchyFacade;
 import net.sourceforge.ganttproject.task.TaskManager;
 import net.sourceforge.ganttproject.task.dependency.TaskDependency;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Map;
 
+import static biz.ganttproject.impex.csv.SpreadsheetFormat.CSV;
+import static biz.ganttproject.impex.csv.SpreadsheetFormat.XLS;
+import static com.google.common.base.Charsets.UTF_8;
+
 /**
- * Tests CSV import with GP semantics.
+ * Tests spreadsheet (CSV and XLS) import with GP semantics.
  *
  * @author dbarashev (Dmitry Barashev)
  */
-public class GPCsvImportTest extends TestCase {
-  private Supplier<Reader> createSupplier(final String data) {
-    return new Supplier<Reader>() {
-      @Override
-      public Reader get() {
-        return new StringReader(data);
-      }
-    };
+public class GPSpreadsheetImportTest extends TestCase {
+
+  private Supplier<InputStream> createSupplier(String data) {
+    return createSupplier(data.getBytes(UTF_8));
+  }
+
+  private Supplier<InputStream> createSupplier(final byte[] data) {
+    return () -> new ByteArrayInputStream(data);
   }
 
   private static Map<String, Task> buildTaskMap(TaskManager taskManager) {
@@ -66,6 +70,10 @@ public class GPCsvImportTest extends TestCase {
         return input.getName();
       }
     });
+  }
+
+  private Map<String, HumanResource> buildResourceMap(HumanResourceManager resourceManager) {
+    return Maps.uniqueIndex(resourceManager.getResources(), (HumanResource input) -> getName());
   }
 
   private static void assertDependency(Task dependant, Task dependee) {
@@ -99,65 +107,77 @@ public class GPCsvImportTest extends TestCase {
   }
 
   public void testImportAssignments() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-    HumanResourceManager resourceManager = builder.getResourceManager();
-    RoleManager roleManager = new RoleManagerImpl();
-
     String header1 = "Name,Begin date,End date,Resources,Duration,Completion,Web Link,Notes,Predecessors,ID";
     String data1 = "t1,23/07/12,25/07/12,Joe;John,,,,,,";
 
     String header2 = "Name,ID,e-mail,Phone,Default role";
     String data2 = "Joe,1,,,\nJohn,2,,,\nJack,3,,,";
-    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, "", header2, data2)),
-        taskManager, resourceManager, roleManager, builder.getTimeUnitStack());
-    importer.load();
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
+    HumanResourceManager resourceManager = builder.getResourceManager();
+    RoleManager roleManager = new RoleManagerImpl();
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, "", header2, data2)),
+        taskManager, resourceManager, roleManager, builder.getTimeUnitStack(), CSV);
+    importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
     Task t1 = taskMap.get("t1");
     assertNotNull(t1);
-    Map<String, HumanResource> resourceMap = Maps.uniqueIndex(resourceManager.getResources(), new Function<HumanResource, String>() {
-      @Override
-      public String apply(HumanResource input) {
-        return input.getName();
-      }
-    });
+    Map<String, HumanResource> resourceMap = buildResourceMap(resourceManager);
+    assertNotNull(t1.getAssignmentCollection().getAssignment(resourceMap.get("Joe")));
+    assertNotNull(t1.getAssignmentCollection().getAssignment(resourceMap.get("John")));
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    resourceManager = builder.getResourceManager();
+    roleManager = new RoleManagerImpl();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, "", header2, data2)),
+        taskManager, resourceManager, roleManager, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
+    t1 = taskMap.get("t1");
+    assertNotNull(t1);
+    resourceMap = buildResourceMap(resourceManager);
     assertNotNull(t1.getAssignmentCollection().getAssignment(resourceMap.get("Joe")));
     assertNotNull(t1.getAssignmentCollection().getAssignment(resourceMap.get("John")));
   }
 
   public void testImportResourceRole() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-    HumanResourceManager resourceManager = builder.getResourceManager();
-    RoleManager roleManager = new RoleManagerImpl();
-
     String header1 = "Name,Begin date,End date,Resources,Duration,Completion,Web Link,Notes,Predecessors,ID";
     String data1 = "";
 
     String header2 = "Name,ID,Default role";
     String data2 = "Joe,1,Default:1";
-    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, "", header2, data2)),
-        taskManager, resourceManager, roleManager, builder.getTimeUnitStack());
-    importer.load();
 
-    Map<String, HumanResource> resourceMap = Maps.uniqueIndex(resourceManager.getResources(), new Function<HumanResource, String>() {
-      @Override
-      public String apply(HumanResource input) {
-        return input.getName();
-      }
-    });
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
+    HumanResourceManager resourceManager = builder.getResourceManager();
+    RoleManager roleManager = new RoleManagerImpl();
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, "", header2, data2)),
+        taskManager, resourceManager, roleManager, builder.getTimeUnitStack(), CSV);
+    importer.load();
+    Map<String, HumanResource> resourceMap = buildResourceMap(resourceManager);
+    assertEquals("project manager", resourceMap.get("Joe").getRole().getName());
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    resourceManager = builder.getResourceManager();
+    roleManager = new RoleManagerImpl();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, "", header2, data2)),
+        taskManager, resourceManager, roleManager, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    resourceMap = buildResourceMap(resourceManager);
     assertEquals("project manager", resourceMap.get("Joe").getRole().getName());
   }
 
   public void testCustomFields() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
     String header1 = "Field1,ID,Name,Begin date,End date,Predecessors,Resources,Duration,Completion,Web Link,Notes,Field2";
     String data1 = "value1,,t1,23/07/12,25/07/12,,,,,,,value2";
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
     GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1)),
-        taskManager, null, null, builder.getTimeUnitStack());
+        taskManager, null, null, builder.getTimeUnitStack(), CSV);
     importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
     Task t1 = taskMap.get("t1");
@@ -170,12 +190,26 @@ public class GPCsvImportTest extends TestCase {
 
     assertEquals("value1", t1.getCustomValues().getValue(def1));
     assertEquals("value2", t1.getCustomValues().getValue(def2));
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1)),
+        taskManager, null, null, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
+    t1 = taskMap.get("t1");
+    assertNotNull(t1);
+
+    def1 = taskManager.getCustomPropertyManager().getCustomPropertyDefinition("Field1");
+    def2 = taskManager.getCustomPropertyManager().getCustomPropertyDefinition("Field2");
+    assertNotNull(def1);
+    assertNotNull(def2);
+
+    assertEquals("value1", t1.getCustomValues().getValue(def1));
+    assertEquals("value2", t1.getCustomValues().getValue(def2));
   }
 
   public void testDependencies() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-
     String header1 = "ID,Name,Begin date,End date,Resources,Duration,Completion,Web Link,Notes,Predecessors";
     String data1 = "1,t1,23/07/12,25/07/12,,,,,,";
     String data2 = "2,t2,26/07/12,27/07/12,,,,,,1";
@@ -183,8 +217,10 @@ public class GPCsvImportTest extends TestCase {
     String data4 = "4,t4,26/07/12,30/07/12,,,,,,1-FS=P1D";
     String data5 = "5,t5,26/07/12,30/07/12,,,,,,1-FS=P-1D";
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
     GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, data2, data3, data4, data5)),
-        taskManager, null, null, builder.getTimeUnitStack());
+        taskManager, null, null, builder.getTimeUnitStack(), CSV);
     importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
     Task t1 = taskMap.get("t1");
@@ -197,30 +233,54 @@ public class GPCsvImportTest extends TestCase {
     assertDependency(t3, t1);
     assertDependency(t4, t1, 1);
     assertDependency(t5, t1, -1);
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, data2, data3, data4, data5)),
+        taskManager, null, null, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
+    t1 = taskMap.get("t1");
+    t2 = taskMap.get("t2");
+    t3 = taskMap.get("t3");
+    t4 = taskMap.get("t4");
+    t5 = taskMap.get("t5");
+
+    assertDependency(t2, t1);
+    assertDependency(t3, t1);
+    assertDependency(t4, t1, 1);
+    assertDependency(t5, t1, -1);
   }
 
   public void testMilestone() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-
     String header1 = "ID,Name,Begin date,End date,Duration,Resources,Completion,Web Link,Notes,Predecessors";
     String data1 = "1,t1,23/07/12,24/07/12,1,,,,,";
     String data2 = "2,t2,26/07/12,26/07/12,0,,,,,";
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
     GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, data2)),
-        taskManager, null, null, builder.getTimeUnitStack());
+        taskManager, null, null, builder.getTimeUnitStack(), CSV);
     importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
     Task t1 = taskMap.get("t1");
     Task t2 = taskMap.get("t2");
     assertFalse(t1.isMilestone());
     assertTrue(t2.isMilestone());
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, data2)),
+        taskManager, null, null, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
+    t1 = taskMap.get("t1");
+    t2 = taskMap.get("t2");
+    assertFalse(t1.isMilestone());
+    assertTrue(t2.isMilestone());
   }
 
-  public void testHierarchy() throws IOException {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-
+  public void testHierarchy() throws Exception {
     String header1 = "ID,Name,Begin date,End date,Duration,Outline number";
     String data1 = "1,t1,23/07/12,26/07/12,1,1";
     String data2 = "2,t2,23/07/12,24/07/12,1,1.1";
@@ -228,8 +288,10 @@ public class GPCsvImportTest extends TestCase {
     String data4 = "4,t4,24/07/12,25/07/12,1,1.2.1";
     String data5 = "5,t5,25/07/12,26/07/12,1,1.2.2";
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
     GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, data2, data3, data4, data5)),
-        taskManager, null, null, builder.getTimeUnitStack());
+        taskManager, null, null, builder.getTimeUnitStack(), CSV);
     importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
     TaskContainmentHierarchyFacade hierarchy = taskManager.getTaskHierarchy();
@@ -242,19 +304,43 @@ public class GPCsvImportTest extends TestCase {
     assertEquals(t3, hierarchy.getContainer(t4));
     assertEquals(t1, hierarchy.getContainer(t3));
     assertEquals(t1, hierarchy.getContainer(t2));
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, data2, data3, data4, data5)),
+        taskManager, null, null, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
+    hierarchy = taskManager.getTaskHierarchy();
+    t1 = taskMap.get("t1");
+    t2 = taskMap.get("t2");
+    t3 = taskMap.get("t3");
+    t4 = taskMap.get("t4");
+    t5 = taskMap.get("t5");
+    assertEquals(t3, hierarchy.getContainer(t5));
+    assertEquals(t3, hierarchy.getContainer(t4));
+    assertEquals(t1, hierarchy.getContainer(t3));
+    assertEquals(t1, hierarchy.getContainer(t2));
   }
 
   public void testUseEndDateInsteadOfDuration() throws Exception {
-    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
-    TaskManager taskManager = builder.build();
-
     String header1 = "ID,Name,Begin date,End date";
     String data1 = "1,t1,23/07/12,26/07/12";
 
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
     GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1)),
-        taskManager, null, null, builder.getTimeUnitStack());
+        taskManager, null, null, builder.getTimeUnitStack(), CSV);
     importer.load();
     Map<String, Task> taskMap = buildTaskMap(taskManager);
+    assertEquals(4.0f, taskMap.get("t1").getDuration().getLength(builder.getTimeUnitStack().getDefaultTimeUnit()));
+
+    builder = TestSetupHelper.newTaskManagerBuilder();
+    taskManager = builder.build();
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1)),
+        taskManager, null, null, builder.getTimeUnitStack(), XLS);
+    importer.load();
+    taskMap = buildTaskMap(taskManager);
     assertEquals(4.0f, taskMap.get("t1").getDuration().getLength(builder.getTimeUnitStack().getDefaultTimeUnit()));
   }
 
@@ -264,6 +350,7 @@ public class GPCsvImportTest extends TestCase {
     assertEquals(0, TaskRecords.OUTLINE_NUMBER_COMPARATOR.compare(first, first));
     assertEquals(0, TaskRecords.OUTLINE_NUMBER_COMPARATOR.compare(second, second));
   }
+
   public void testOutlineNumberComparator() {
     assertOrder("1", "2");
     assertOrder("1", "1.1");
@@ -271,5 +358,18 @@ public class GPCsvImportTest extends TestCase {
     assertOrder("1.1", "2");
     assertOrder("1.2", "1.10");
     assertOrder("2", "10");
+  }
+
+  private byte[] createXls(String... rows) throws Exception {
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    try (SpreadsheetWriter writer = new XlsWriterImpl(stream)) {
+      for (String row : rows) {
+        for (String cel : row.split(",")) {
+          writer.print(cel.trim());
+        }
+        writer.println();
+      }
+    }
+    return stream.toByteArray();
   }
 }
---
+++ ganttproject-tester/test/biz/ganttproject/impex/csv/SpreadsheetImportTest.java
@@ -0,0 +1,275 @@
+/*
+GanttProject is an opensource project management tool. License: GPL3
+Copyright (C) 2012 GanttProject Team
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+package biz.ganttproject.impex.csv;
+
+import java.io.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Supplier;
+import com.google.common.collect.ImmutableSet;
+
+import junit.framework.TestCase;
+
+import static biz.ganttproject.impex.csv.SpreadsheetFormat.CSV;
+import static biz.ganttproject.impex.csv.SpreadsheetFormat.XLS;
+import static com.google.common.base.Charsets.UTF_8;
+
+/**
+ * Tests for spreadsheet (CSV and XLS) import.
+ *
+ * @author dbarashev (Dmitry Barashev)
+ */
+public class SpreadsheetImportTest extends TestCase {
+
+  private Supplier<InputStream> createSupplier(String data) {
+    return createSupplier(data.getBytes(UTF_8));
+  }
+
+  private Supplier<InputStream> createSupplier(final byte[] data) {
+    return () -> new ByteArrayInputStream(data);
+  }
+
+  public void testBasic() throws Exception {
+    String header = "A, B";
+    String data = "a1, b1";
+
+    AtomicBoolean wasCalled = new AtomicBoolean(false);
+    RecordGroup recordGroup = createBasicRecordGroup(wasCalled);
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header, data)), CSV, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+
+    wasCalled = new AtomicBoolean(false);
+    recordGroup = createBasicRecordGroup(wasCalled);
+    importer = new GanttCSVOpen(createSupplier(createXls(header, data)), XLS, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+  }
+
+  public void testSkipEmptyLine() throws Exception {
+    String header = "A, B";
+    String data = "a1, b1";
+
+    AtomicBoolean wasCalled = new AtomicBoolean(false);
+    RecordGroup recordGroup = createBasicRecordGroup(wasCalled);
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header, "", data)), CSV, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+
+    wasCalled = new AtomicBoolean(false);
+    recordGroup = createBasicRecordGroup(wasCalled);
+    importer = new GanttCSVOpen(createSupplier(createXls(header, "", data)), XLS, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+  }
+
+  public void testTwoGroups() throws Exception {
+    String header1 = "A, B";
+    String data1 = "a1, b1";
+    AtomicBoolean wasCalled1 = new AtomicBoolean(false);
+    RecordGroup recordGroup1 = createTwoGroupsFirstRecordGroup(wasCalled1);
+
+    String header2 = "C, D, E";
+    String data2 = "c1, d1, e1";
+    AtomicBoolean wasCalled2 = new AtomicBoolean(false);
+    RecordGroup recordGroup2 = createTwoGroupsSecondRecordGroup(wasCalled2);
+
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1, "", header2, data2)),
+        CSV, recordGroup1, recordGroup2);
+    importer.load();
+    assertTrue(wasCalled1.get() && wasCalled2.get());
+
+    wasCalled1 = new AtomicBoolean(false);
+    recordGroup1 = createTwoGroupsFirstRecordGroup(wasCalled1);
+    wasCalled2 = new AtomicBoolean(false);
+    recordGroup2 = createTwoGroupsSecondRecordGroup(wasCalled2);
+    importer = new GanttCSVOpen(createSupplier(createXls(header1, data1, "", header2, data2)),
+        XLS, recordGroup1, recordGroup2);
+    importer.load();
+    assertTrue(wasCalled1.get() && wasCalled2.get());
+  }
+
+  public void testIncompleteHeader() throws Exception {
+    String header = "A, B";
+    String data = "a1, b1";
+
+    AtomicBoolean wasCalled = new AtomicBoolean(false);
+    RecordGroup recordGroup = createIncompleteHeaderRecordGroup(wasCalled);
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header, data)), CSV, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+
+    wasCalled = new AtomicBoolean(false);
+    recordGroup = createIncompleteHeaderRecordGroup(wasCalled);
+    importer = new GanttCSVOpen(createSupplier(createXls(header, data)), XLS, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+  }
+
+  public void testSkipUntilFirstHeader() throws Exception {
+    String notHeader = "FOO, BAR, A";
+    String header = "A, B";
+    String data = "a1, b1";
+
+    AtomicBoolean wasCalled = new AtomicBoolean(false);
+    RecordGroup recordGroup = createSkipUntilFirstHeaderRecordGroup(wasCalled);
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(notHeader, header, data)), CSV, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+    assertEquals(1, importer.getSkippedLineCount());
+
+    wasCalled = new AtomicBoolean(false);
+    recordGroup = createSkipUntilFirstHeaderRecordGroup(wasCalled);
+    importer = new GanttCSVOpen(createSupplier(createXls(notHeader, header, data)), XLS, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+    assertEquals(1, importer.getSkippedLineCount());
+  }
+
+  public void testSkipLinesWithEmptyMandatoryFields() throws Exception {
+    String header = "A, B, C";
+    String data1 = "a1,,c1";
+    String data2 = "a2,b2,c2";
+    String data3 = ",b3,c3";
+
+    AtomicBoolean wasCalled = new AtomicBoolean(false);
+    RecordGroup recordGroup = createSkipLinesWithEmptyMandatoryFieldsRecordGroup(wasCalled);
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header, data1, data2, data3)), CSV, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+    assertEquals(2, importer.getSkippedLineCount());
+
+    wasCalled = new AtomicBoolean(false);
+    recordGroup = createSkipLinesWithEmptyMandatoryFieldsRecordGroup(wasCalled);
+    importer = new GanttCSVOpen(createSupplier(createXls(header, data1, data2, data3)), XLS, recordGroup);
+    importer.load();
+    assertTrue(wasCalled.get());
+    assertEquals(2, importer.getSkippedLineCount());
+  }
+
+  private RecordGroup createBasicRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("AB", ImmutableSet.of("A", "B")) {
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        wasCalled.set(true);
+        assertEquals("a1", record.get("A"));
+        assertEquals("b1", record.get("B"));
+        return true;
+      }
+    };
+  }
+
+  private RecordGroup createTwoGroupsFirstRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("AB", ImmutableSet.of("A", "B")) {
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        assertEquals("a1", record.get("A"));
+        assertEquals("b1", record.get("B"));
+        wasCalled.set(true);
+        return true;
+      }
+    };
+  }
+
+  private RecordGroup createTwoGroupsSecondRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("CDE", ImmutableSet.of("C", "D", "E")) {
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        assertEquals("c1", record.get("C"));
+        assertEquals("d1", record.get("D"));
+        assertEquals("e1", record.get("E"));
+        wasCalled.set(true);
+        return true;
+      }
+    };
+  }
+
+  private RecordGroup createIncompleteHeaderRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("ABC",
+        ImmutableSet.of("A", "B", "C"), // all fields
+        ImmutableSet.of("A", "B")) { // mandatory fields
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        wasCalled.set(true);
+        assertEquals("a1", record.get("A"));
+        assertEquals("b1", record.get("B"));
+        return true;
+      }
+    };
+  }
+
+  private RecordGroup createSkipUntilFirstHeaderRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("ABC", ImmutableSet.of("A", "B")) {
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        wasCalled.set(true);
+        assertEquals("a1", record.get("A"));
+        assertEquals("b1", record.get("B"));
+        return true;
+      }
+    };
+  }
+
+  private RecordGroup createSkipLinesWithEmptyMandatoryFieldsRecordGroup(final AtomicBoolean wasCalled) {
+    return new RecordGroup("ABC", ImmutableSet.of("A", "B", "C"), ImmutableSet.of("A", "B")) {
+      @Override
+      protected boolean doProcess(SpreadsheetRecord record) {
+        if (!super.doProcess(record)) {
+          return false;
+        }
+        if (!hasMandatoryFields(record)) {
+          return false;
+        }
+        wasCalled.set(true);
+        assertEquals("a2", record.get("A"));
+        assertEquals("b2", record.get("B"));
+        return true;
+      }
+    };
+  }
+
+  private byte[] createXls(String... rows) throws Exception {
+    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+    try (SpreadsheetWriter writer = new XlsWriterImpl(stream)) {
+      for (String row : rows) {
+        for (String cel : row.split(",")) {
+          writer.print(cel.trim());
+        }
+        writer.println();
+      }
+    }
+    return stream.toByteArray();
+  }
+}
