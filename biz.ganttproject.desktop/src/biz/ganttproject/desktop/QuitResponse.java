84813b5053b708a68b77536ecbf3e171aa6a79b6
Dmitry Barashev <dbarashev@ganttproject.biz>
2018-05-08 17:43:38 +0300

    Desktop integration code for Java 9+ (replaces AppleJavaExtensions). It uses Java 9+ API but JAR is compiled to Java 7 bytecode to be compatible with the rest of GanttProject. Update issue #1530

---
+++ biz.ganttproject.desktop/src/biz/ganttproject/desktop/QuitResponse.java
@@ -0,0 +1,10 @@
+// Copyright (C) 2018 BarD Software
+package biz.ganttproject.desktop;
+
+/**
+ * @author dbarashev@bardsoftware.com
+ */
+public interface QuitResponse {
+  void performQuit();
+  void cancelQuit();
+}
---
+++ ganttproject/src/net/sourceforge/ganttproject/DesktopIntegration.java
@@ -0,0 +1,8 @@
+// Copyright (C) 2018 BarD Software
+package net.sourceforge.ganttproject;
+
+/**
+ * @author dbarashev@bardsoftware.com
+ */
+public class DesktopIntegration {
+}
