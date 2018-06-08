5b3f4abad158059b2ddc074ab5430c96aaab6336
Dmitry Barashev <dbarashev@ganttproject.biz>
2018-06-09 02:48:01 +0300

    Ignore empty duration value Update issue #1556

--- ganttproject-tester/test/biz/ganttproject/impex/csv/GPCsvImportTest.java
+++ ganttproject-tester/test/biz/ganttproject/impex/csv/GPCsvImportTest.java
@@ -295,6 +295,20 @@ public class GPCsvImportTest extends TestCase {
     assertEquals(4.0f, taskMap.get("t1").getDuration().getLength(builder.getTimeUnitStack().getDefaultTimeUnit()));
   }
 
+  public void testEmptyDuration() throws Exception {
+    TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+    TaskManager taskManager = builder.build();
+
+    String header1 = "ID,Name,Begin date,Duration,End date";
+    String data1 = "1,t1,23/07/12,,26/07/12";
+
+    GanttCSVOpen importer = new GanttCSVOpen(createSupplier(Joiner.on('\n').join(header1, data1)),
+        taskManager, null, null, builder.getTimeUnitStack());
+    importer.load();
+    Map<String, Task> taskMap = buildTaskMap(taskManager);
+    assertEquals(4.0f, taskMap.get("t1").getDuration().getLength(builder.getTimeUnitStack().getDefaultTimeUnit()));
+  }
+
   private static void assertOrder(String first, String second) {
     assertEquals(-1, TaskRecords.OUTLINE_NUMBER_COMPARATOR.compare(first, second));
     assertEquals(1, TaskRecords.OUTLINE_NUMBER_COMPARATOR.compare(second, first));
