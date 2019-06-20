2da4b11951e3b1cab4fd7d6276d85fe0ec38b3f4
Dmitry Barashev <dbarashev@ganttproject.biz>
2019-06-20 21:56:01 +0300

    What could go wrong?

--- build-bin/package-win.bat
+++ build-bin/package-win.bat
@@ -10,27 +10,6 @@ set VERSION=%3
   --strip-debug ^
   --compress=2
 
+dir
 
-"C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64\bin\java" --module-path build-bin\win ^
-  --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
-  -m jdk.packager/jdk.packager.Main ^
-  create-installer ^
-    --verbose ^
-    --echo-mode ^
-    --name GanttProject ^
-    --output "%OUTPUT%\dist" ^
-    --input "%INPUT%" ^
-    --main-jar eclipsito.jar ^
-    --class com.bardsoftware.eclipsito.Launch ^
-    --version %VERSION% ^
-    --arguments "--verbosity 4 --version-dirs plugins --app net.sourceforge.ganttproject.GanttProject"  ^
-    --runtime-image "%OUTPUT%\runtime" ^
-    --identifier biz.ganttproject ^
-    --description "Free desktop project scheduling and project management application" ^
-    --category "Office" ^
-    --copyright "Copyright 2019 BarD Software s.r.o" ^
-    --vendor "BarD Software s.r.o" ^
-    --license-file LICENSE ^
-    --file-associations build-cfg\file-associations.properties ^
-    --icon build-cfg\ganttproject.ico ^
-    --win-dir-chooser --win-menu
+"C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64\bin\java" -version
