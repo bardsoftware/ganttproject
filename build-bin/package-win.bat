bc0b42f90b10dd5ae34fa081a2a010bfec85a683
Dmitry Barashev <dbarashev@ganttproject.biz>
2019-06-20 22:12:11 +0300

    Updated paths to Java 11

--- build-bin/package-win.bat
+++ build-bin/package-win.bat
@@ -3,7 +3,7 @@ set INPUT=%2
 set OUTPUT=%1
 set VERSION=%3
 
-"C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64\bin\jlink" ^
+"C:\Program Files\Java\zulu-11-azure-jdk_11.31.11-11.0.3-win_x64\bin\jlink" ^
   --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.naming,java.net.http,java.security.jgss,java.xml,jdk.charsets,jdk.unsupported ^
   --no-header-files --no-man-pages ^
   --output "%OUTPUT%\runtime" ^
@@ -11,7 +11,31 @@ set VERSION=%3
   --compress=2
 
 dir "C:\Program Files\Java"
-dir "C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64"
-dir "C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64\bin"
+dir "C:\Program Files\Java\zulu-11-azure-jdk_11.31.11-11.0.3-win_x64"
+dir "C:\Program Files\Java\zulu-11-azure-jdk_11.31.11-11.0.3-win_x64\bin"
 
-"C:\Program Files\Java\zulu-11-azure-jdk_11.29.3-11.0.2-win_x64\bin\java" -version
+"C:\Program Files\Java\zulu-11-azure-jdk_11.31.11-11.0.3-win_x64\binjava" -version
+
+"C:\Program Files\Java\zulu-11-azure-jdk_11.31.11-11.0.3-win_x64\bin\java" --module-path build-bin\win ^
+  --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
+  -m jdk.packager/jdk.packager.Main ^
+  create-installer ^
+    --verbose ^
+    --echo-mode ^
+    --name GanttProject ^
+    --output "%OUTPUT%\dist" ^
+    --input "%INPUT%" ^
+    --main-jar eclipsito.jar ^
+    --class com.bardsoftware.eclipsito.Launch ^
+    --version %VERSION% ^
+    --arguments "--verbosity 4 --version-dirs plugins --app net.sourceforge.ganttproject.GanttProject"  ^
+    --runtime-image "%OUTPUT%\runtime" ^
+    --identifier biz.ganttproject ^
+    --description "Free desktop project scheduling and project management application" ^
+    --category "Office" ^
+    --copyright "Copyright 2019 BarD Software s.r.o" ^
+    --vendor "BarD Software s.r.o" ^
+    --license-file LICENSE ^
+    --file-associations build-cfg\file-associations.properties ^
+    --icon build-cfg\ganttproject.ico ^
+    --win-dir-chooser --win-menu
