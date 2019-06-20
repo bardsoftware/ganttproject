c5ae1f3198696e0bd6eca6417334f0c97055cabe
Dmitry Barashev <dbarashev@ganttproject.biz>
2019-06-20 21:11:20 +0300

    * Azure task to build Windows package * Fixes Travis build

--- build-bin/package-win.bat
+++ build-bin/package-win.bat
@@ -1,28 +1,24 @@
 
 set INPUT=%2
 set OUTPUT=%1
-set FILE_ASSOCIATIONS=%4
 set VERSION=%3
-set APP_ICON=%5
 
-set MODULE_PATH=%JAVA_HOME%\jmods
-call "%JAVA_HOME%\bin\jlink.exe" ^
+jlink ^
   --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.naming,java.net.http,java.security.jgss,java.xml,jdk.charsets,jdk.unsupported ^
-  --module-path "%MODULE_PATH%" ^
   --no-header-files --no-man-pages ^
   --output "%OUTPUT%\runtime" ^
   --strip-debug ^
   --compress=2
 
 
-java --module-path "%JAVA_HOME%\bin" ^
+java --module-path build-bin\win ^
   --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
   -m jdk.packager/jdk.packager.Main ^
   create-installer ^
     --verbose ^
     --echo-mode ^
     --name GanttProject ^
-    --output "%OUTPUT%/dist" ^
+    --output "%OUTPUT%\dist" ^
     --input "%INPUT%" ^
     --main-jar eclipsito.jar ^
     --class com.bardsoftware.eclipsito.Launch ^
--- ganttproject-builder/build-travis.sh
+++ ganttproject-builder/build-travis.sh
@@ -2,6 +2,6 @@
 find . -path */mvn/* -exec rm '{}' ';'
 git pull --tags origin
 ./gradlew distbin
-build-bin/build-package.sh build ganttproject-builder/dist-bin/ 2.99.0
+build-bin/package-lin.sh build ganttproject-builder/dist-bin/ 2.99.0
 ls -l build/dist
- 
+
