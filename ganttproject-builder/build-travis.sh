ae5537d5c1b2458f9375d1d2f9226a103cc1b8b0
Dmitry Barashev <dbarashev@ganttproject.biz>
2019-06-26 18:48:36 +0300

    added submodule

--- ganttproject-builder/build-travis.sh
+++ ganttproject-builder/build-travis.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 find . -path */mvn/* -exec rm '{}' ';'
 git pull --tags origin
+git submodule update --init
 ./gradlew distbin
 build-bin/package-lin.sh build ganttproject-builder/dist-bin/ 2.99.0
 ls -l build/dist
