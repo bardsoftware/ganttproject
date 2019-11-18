d9cb3baaa2257c934a8962b310d306a58b3e4638
Krasnoriadtseva Anastasiia <anstkras@gmail.com>
2019-11-18 20:00:41 +0300

    use Clikt library to process path argument

--- com.bardsoftware.constable/src/main/kotlin/com/bardsoftware/constable/Main.kt
+++ com.bardsoftware.constable/src/main/kotlin/com/bardsoftware/constable/Main.kt
@@ -1,14 +1,19 @@
 package com.bardsoftware.constable
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.validate
 import java.io.File
 
-fun main() {
-    println("Enter path to sql file with database schema to load")
-    val path = readLine()!!
-    if (!File(path).exists()) {
-        println("File does not exist")
-        return
+class H2Client : CliktCommand() {
+    private val path by argument(help = "Path to sql file to load").validate {
+        require(File(it).exists()) { "File does not exist" }
     }
-    val h2Manager = H2Manager()
-    h2Manager.execute(path)
+
+    override fun run() {
+        val h2Manager = H2Manager()
+        h2Manager.execute(path)
+    }
 }
+
+fun main(args: Array<String>) = H2Client().main(args)
