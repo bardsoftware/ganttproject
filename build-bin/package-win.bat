cba34ba6033b64fc8e6aae34827d5167b746b388
Dmitry Barashev <dbarashev@ganttproject.biz>
2020-01-10 03:53:12 +0300

    try using jpackage from jdk14

--- build-bin/package-win.bat
+++ build-bin/package-win.bat
@@ -4,7 +4,7 @@ set OUTPUT=%1
 set VERSION=%3
 set JAVAFX_MODS_PATH=%4
 jlink ^
-  --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.naming,java.net.http,java.security.jgss,java.xml,jdk.charsets,jdk.unsupported,jdk.unsupported.desktop,javafx.controls,javafx.swing,javafx.web,jdk.jlink ^
+  --add-modules java.base,java.datatransfer,java.desktop,java.logging,java.naming,java.net.http,java.security.jgss,java.xml,jdk.charsets,jdk.unsupported,jdk.unsupported.desktop,javafx.controls,javafx.swing,javafx.web ^
   --no-header-files --no-man-pages ^
   --output "%OUTPUT%\runtime" ^
   --strip-debug ^
@@ -12,13 +12,9 @@ jlink ^
 
 dir "%OUTPUT%\runtime\bin"
 
-SET JAVA_HOME="%GITHUB_WORKSPACE%\build\runtime"
-cp build-bin\win\jpackager.exe build\runtime\bin
+jdk-14/bin/jpackage -h
 
-build\runtime\bin\java --module-path "build-bin\win" ^
-  --add-modules jdk.jlink ^
-  --add-opens jdk.jlink/jdk.tools.jlink.internal.packager=jdk.packager ^
-  -m jdk.packager/jdk.packager.Main ^
+jdk-14/bin/jpackage ^
   create-installer ^
     --verbose ^
     --echo-mode ^
@@ -33,7 +29,7 @@ build\runtime\bin\java --module-path "build-bin\win" ^
     --identifier biz.ganttproject ^
     --description "Free desktop project scheduling and project management application" ^
     --category "Office" ^
-    --copyright "Copyright 2019 BarD Software s.r.o" ^
+    --copyright "Copyright 2020 BarD Software s.r.o" ^
     --vendor "BarD Software s.r.o" ^
     --license-file LICENSE ^
     --file-associations build-cfg\file-associations.properties ^
