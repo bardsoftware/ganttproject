d8a59f391461bcfd51feefcb1f8f2a80c852293f
Dmitry Barashev <dbarashev@bardsoftware.com>
2020-04-16 02:30:41 +0300

    * version update * Eclipsito update

--- ganttproject/src/net/sourceforge/ganttproject/GPVersion.java
+++ ganttproject/src/net/sourceforge/ganttproject/GPVersion.java
@@ -19,7 +19,7 @@ along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
 package net.sourceforge.ganttproject;
 
 public abstract class GPVersion {
-  public static String BUILD = "2364"; // BUILD NUMBER
+  public static String BUILD = "2387"; // BUILD NUMBER
   public static String V2_0_1 = "2.0.1";
   public static String V2_0_2 = "2.0.2";
   public static String V2_0_3 = "2.0.3";
@@ -52,8 +52,9 @@ public abstract class GPVersion {
   public static String PILSEN_2_8_7 = String.format("2.8.7 Pilsen (build 2262)");
   public static String PILSEN_2_8_8 = String.format("2.8.8 Pilsen (build 2308)");
   public static String PILSEN_2_8_9 = String.format("2.8.9 Pilsen (build 2335)");
-  public static String PILSEN_2_8_10 = String.format("2.8.10 Pilsen (build %s)", BUILD);
-  public static String CURRENT = PILSEN_2_8_10;
+  public static String PILSEN_2_8_10 = String.format("2.8.10 Pilsen (build 2364)");
+  public static String PILSEN_2_8_11 = String.format("2.8.11 Pilsen (build %s)", BUILD);
+  public static String CURRENT = PILSEN_2_8_11;
 
   public static String getCurrentVersionNumber() {
     return CURRENT.split("\\s")[0];
