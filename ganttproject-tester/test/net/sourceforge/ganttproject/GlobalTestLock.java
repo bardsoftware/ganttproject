86ac226bd1490234907fc858a99ad73c75588f7f
larsvanroy <s0161083@ad.ua.ac.be>
2020-05-27 11:21:14 +0200

    added timers to check whether is the ui is up

--- ganttproject-tester/test/net/sourceforge/ganttproject/GlobalTestLock.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/GlobalTestLock.java
@@ -1,21 +1,45 @@
 package net.sourceforge.ganttproject;
 
 import junit.framework.TestCase;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public abstract class GlobalTestLock extends TestCase {
     public GanttProject myproject;
     static final AtomicBoolean turn = new AtomicBoolean(true);
+    protected final CountDownLatch conditionSucceeded = new CountDownLatch(1);
+    private Timer timer = new Timer();
+
+    static class CheckConditionTurn extends TimerTask{
+        CountDownLatch latch;
+
+        public CheckConditionTurn(CountDownLatch latch){
+            this.latch = latch;
+        }
+
+        public void run(){
+            if(AppKt.getMainWindow().get() != null){
+                latch.countDown();
+            }
+        }
+    }
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-
-        while(!GlobalTestLock.turn.compareAndSet(true, false)){}
-        AppKt.main(new String[]{});
-        while(myproject == null) {
-            myproject = AppKt.getMainWindow().get();
+        boolean success = false;
+        while (!GlobalTestLock.turn.compareAndSet(true, false)) {}
+        while(!success){
+            AppKt.main(new String[]{});
+            CheckConditionTurn task = new CheckConditionTurn(conditionSucceeded);
+            timer.schedule(task, 0, 2000);
+            success = conditionSucceeded.await(3, TimeUnit.SECONDS);
         }
+        myproject = AppKt.getMainWindow().get();
     }
 
     @Override
