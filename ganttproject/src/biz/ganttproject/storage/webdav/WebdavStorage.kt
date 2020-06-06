38fab25053186dcb1045a250d36276f9aa2a1c24
Dmitry Barashev <dbarashev@ganttproject.biz>
2020-06-06 21:23:13 +0300

    Refactoring: moved webdav browser to the separate file

--- ganttproject/src/biz/ganttproject/storage/webdav/WebdavStorage.kt
+++ ganttproject/src/biz/ganttproject/storage/webdav/WebdavStorage.kt
@@ -2,18 +2,11 @@
 package biz.ganttproject.storage.webdav
 
 import biz.ganttproject.FXUtil
-import biz.ganttproject.app.RootLocalizer
 import biz.ganttproject.storage.*
 import biz.ganttproject.storage.cloud.GPCloudStorageOptions
 import com.google.common.base.Strings
-import javafx.beans.property.SimpleBooleanProperty
-import javafx.collections.FXCollections
-import javafx.collections.ObservableList
-import javafx.event.ActionEvent
-import javafx.event.EventHandler
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.Pane
-import net.sourceforge.ganttproject.GPLogger
 import net.sourceforge.ganttproject.document.Document
 import net.sourceforge.ganttproject.document.webdav.HttpDocument
 import net.sourceforge.ganttproject.document.webdav.WebDavResource
@@ -21,37 +14,6 @@ import net.sourceforge.ganttproject.document.webdav.WebDavServerDescriptor
 import java.util.*
 import java.util.function.Consumer
 
-class WebDavResourceAsFolderItem(val myResource: WebDavResource) : FolderItem {
-  override val isLocked: Boolean
-    get() {
-      try {
-        return myResource.isLocked
-      } catch (e: WebDavResource.WebDavException) {
-        throw RuntimeException(e)
-      }
-
-    }
-
-  override val isLockable: Boolean
-    get() = myResource.isLockSupported(true)
-
-  override val canChangeLock: Boolean
-    get() = isLockable
-
-  override val name: String
-    get() = myResource.name
-
-  override val isDirectory: Boolean
-    get() {
-      try {
-        return myResource.isCollection
-      } catch (e: WebDavResource.WebDavException) {
-        throw RuntimeException(e)
-      }
-
-    }
-  override val tags = listOf<String>()
-}
 
 /**
  * @author dbarashev@bardsoftware.com
@@ -83,7 +45,7 @@ class WebdavStorage(
   override fun createUi(): Pane = myBorderPane.apply { center = doCreateUi() }
 
   private fun createStorageUi(): Pane {
-    val serverUi = WebdavServerUi(myServer, myMode, myOpenDocument, myDialogUi)
+    val serverUi = WebdavBrowserPane(myServer, myMode, myOpenDocument, myDialogUi)
     return serverUi.createStorageUi()
   }
 
@@ -109,124 +71,3 @@ class WebdavStorage(
   }
 }
 
-data class State(val server: WebDavServerDescriptor,
-                 var resource: WebDavResource?, var filename: String?, var folder: WebDavResource?)
-
-
-class WebdavServerUi(private val myServer: WebDavServerDescriptor,
-                     private val myMode: StorageDialogBuilder.Mode,
-                     private val myOpenDocument: (Document) -> Unit,
-                     private val myDialogUi: StorageDialogBuilder.DialogUi) {
-  private val myLoadService: WebdavLoadService = WebdavLoadService(myServer)
-  private val myState = State(server = myServer, resource = null, filename = null, folder = null)
-
-  private fun createResource(state: State): WebDavResource {
-    return state.resource ?: myLoadService.createResource(state.folder, state.filename)
-  }
-
-  fun createStorageUi(): Pane {
-    val builder = BrowserPaneBuilder<WebDavResourceAsFolderItem>(this.myMode, this.myDialogUi::error) { path, success, loading ->
-      val wrappers = FXCollections.observableArrayList<WebDavResourceAsFolderItem>()
-      val consumer = Consumer { webDavResources: ObservableList<WebDavResource> ->
-        webDavResources.forEach { resource -> wrappers.add(WebDavResourceAsFolderItem(resource)) }
-        success.accept(wrappers)
-      }
-      loadFolder(path, loading, consumer, myDialogUi)
-    }
-    val isLockingSupported = SimpleBooleanProperty()
-    isLockingSupported.addListener { _, _, newValue ->
-      System.err.println("is locking supported=" + newValue!!)
-    }
-    builder.apply {
-      withI18N(RootLocalizer.createWithRootKey("storageService.webdav", BROWSE_PANE_LOCALIZER))
-      withBreadcrumbs(DocumentUri(listOf(), true, myServer.name))
-      withListView(
-          onSelectionChange = { item ->
-            if (item.isDirectory) {
-              myState.folder = item.myResource
-              myState.filename = null
-              myState.resource = null
-            } else {
-              myState.resource = item.myResource
-            }
-          },
-          onLaunch = {
-            myOpenDocument(createDocument(myState.server, createResource(myState)))
-          },
-          onNameTyped = { filename, _, withEnter, withControl ->
-            myState.filename = filename
-            if (withEnter && withControl) {
-              myOpenDocument(createDocument(myState.server, createResource(myState)))
-            }
-          },
-          onDelete = { item ->
-            deleteResource(item)
-          },
-          onLock = { item ->
-            toggleLockResource(item)
-          },
-          canLock = isLockingSupported,
-          canDelete = SimpleBooleanProperty(true)
-      )
-
-
-      withActionButton { btn ->
-        btn.addEventHandler(ActionEvent.ACTION) {
-          myOpenDocument(createDocument(myState.server, createResource(myState)))
-        }
-      }
-    }
-    val browserPaneElements = builder.build()
-    return browserPaneElements.browserPane
-  }
-
-  private fun deleteResource(folderItem: WebDavResourceAsFolderItem) {
-    val resource = folderItem.myResource
-    try {
-      resource.delete()
-    } catch (e: WebDavResource.WebDavException) {
-      myDialogUi.error(e)
-    }
-  }
-
-  private fun toggleLockResource(folderItem: WebDavResourceAsFolderItem) {
-    try {
-      val resource = folderItem.myResource
-      if (resource.isLocked) {
-        resource.unlock()
-      } else {
-        resource.lock(-1)
-      }
-    } catch (e: WebDavResource.WebDavException) {
-      myDialogUi.error(e)
-    }
-  }
-
-  private fun loadFolder(selectedPath: Path,
-                         showMaskPane: Consumer<Boolean>,
-                         setResult: Consumer<ObservableList<WebDavResource>>,
-                         dialogUi: StorageDialogBuilder.DialogUi) {
-    myLoadService.setPath(selectedPath.toString())
-    myState.folder = myLoadService.createRootResource()
-    myLoadService.apply {
-      onSucceeded = EventHandler {
-        setResult.accept(value)
-        showMaskPane.accept(false)
-      }
-      onFailed = EventHandler {
-        showMaskPane.accept(false)
-        dialogUi.error("WebdavService failed!")
-      }
-      onCancelled = EventHandler {
-        showMaskPane.accept(false)
-        GPLogger.log("WebdavService cancelled!")
-      }
-      restart()
-    }
-    showMaskPane.accept(true)
-  }
-}
-
-fun createDocument(server: WebDavServerDescriptor, resource: WebDavResource): Document {
-  return HttpDocument(resource, server.getUsername(), server.password, HttpDocument.NO_LOCK)
-}
