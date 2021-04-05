369d7164580dec03956c4a6e2362a901955942be
Emile Dupont-Foisy <emiledupontfoisy@live.ca>
2021-04-05 15:20:20 -0400

    Added copyright notice to TestHumanResourceManager.java.

--- ganttproject-tester/test/net/sourceforge/ganttproject/resource/TestHumanResourceManager.java
+++ ganttproject-tester/test/net/sourceforge/ganttproject/resource/TestHumanResourceManager.java
@@ -1,8 +1,25 @@
+/*
+Copyright 2021 BarD Software s.r.o, Emile Dupont-Foisy
+
+This file is part of GanttProject, an open-source project management tool.
+
+GanttProject is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+GanttProject is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package net.sourceforge.ganttproject.resource;
 
 import junit.framework.TestCase;
 import net.sourceforge.ganttproject.task.CustomColumnsManager;
-
 
 public class TestHumanResourceManager extends TestCase {
   public void testUpResource() {
@@ -24,8 +41,8 @@ public class TestHumanResourceManager extends TestCase {
     assertEquals(0, manager.getResources().indexOf(resource2));
 
   }
+
   public void testDownResource() {
-    // Arrange
     HumanResourceManager manager = new HumanResourceManager(null, new CustomColumnsManager());
     var resource1 = manager.newHumanResource();
     resource1.setName("TEST_RESSOURCE1");
