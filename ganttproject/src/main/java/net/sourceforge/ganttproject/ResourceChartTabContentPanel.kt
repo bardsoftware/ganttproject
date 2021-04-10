5d318f21ef12c79698f0cfc2ff2b64542be43085
Dmitry Barashev <dbarashev@ganttproject.biz>
2021-04-11 02:44:00 +0300

    Converted Java to Kotlin

--- ganttproject/src/main/java/net/sourceforge/ganttproject/ResourceChartTabContentPanel.kt
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/ResourceChartTabContentPanel.kt
@@ -1,66 +1,53 @@
 /*
  * Created on 22.10.2005
  */
-package net.sourceforge.ganttproject;
+package net.sourceforge.ganttproject
 
-import net.sourceforge.ganttproject.chart.Chart;
-import net.sourceforge.ganttproject.chart.overview.GPToolbar;
-import net.sourceforge.ganttproject.chart.overview.ToolbarBuilder;
-import net.sourceforge.ganttproject.gui.UIFacade;
-import net.sourceforge.ganttproject.gui.view.GPView;
+import net.sourceforge.ganttproject.gui.UIFacade
+import net.sourceforge.ganttproject.chart.Chart
+import net.sourceforge.ganttproject.gui.view.GPView
+import javax.swing.JComponent
+import net.sourceforge.ganttproject.chart.overview.ToolbarBuilder
+import java.awt.Component
 
-import javax.swing.*;
-import java.awt.*;
+internal class ResourceChartTabContentPanel(
+  project: IGanttProject, workbenchFacade: UIFacade, private val myTreeFacade: TreeTableContainer<*, *, *>,
+  override val chartComponent: Component
+) : ChartTabContentPanel(project, workbenchFacade, workbenchFacade.resourceChart), GPView {
 
-class ResourceChartTabContentPanel extends ChartTabContentPanel implements GPView {
-  private TreeTableContainer myTreeFacade;
-  private Component myResourceChart;
-  private JComponent myTabContentPanel;
-
-  ResourceChartTabContentPanel(IGanttProject project, UIFacade workbenchFacade, TreeTableContainer resourceTree,
-      Component resourceChart) {
-    super(project, workbenchFacade, workbenchFacade.getResourceChart());
-    myTreeFacade = resourceTree;
-    myResourceChart = resourceChart;
-    addTableResizeListeners(resourceTree.getTreeComponent(), myTreeFacade.getTreeTable().getScrollPane().getViewport());
-  }
-
-  JComponent getComponent() {
-    if (myTabContentPanel == null) {
-      myTabContentPanel = createContentComponent();
+  private var myTabContentPanel: JComponent? = null
+  val component: JComponent
+    get() {
+      if (myTabContentPanel == null) {
+        myTabContentPanel = createContentComponent()
+      }
+      return myTabContentPanel!!
     }
-    return myTabContentPanel;
+
+  override fun createButtonPanel(): Component? {
+    val builder = ToolbarBuilder()
+      .withHeight(24)
+      .withSquareButtons()
+      .withDpiOption(getUiFacade().dpiOption)
+      .withLafOption(getUiFacade().lafOption, null)
+    myTreeFacade.addToolbarActions(builder)
+    val toolbar = builder.build()
+    return toolbar.toolbar
   }
 
-  @Override
-  protected Component createButtonPanel() {
-    ToolbarBuilder builder = new ToolbarBuilder()
-        .withHeight(24)
-        .withSquareButtons()
-        .withDpiOption(getUiFacade().getDpiOption())
-        .withLafOption(getUiFacade().getLafOption(), null);
-    myTreeFacade.addToolbarActions(builder);
-    final GPToolbar toolbar = builder.build();
-    return toolbar.getToolbar();
+  override fun getTreeComponent(): Component {
+    return myTreeFacade.treeComponent
   }
 
-  @Override
-  protected Component getChartComponent() {
-    return myResourceChart;
+  override fun getChart(): Chart {
+    return getUiFacade().resourceChart
   }
 
-  @Override
-  protected Component getTreeComponent() {
-    return myTreeFacade.getTreeComponent();
+  override fun getViewComponent(): Component {
+    return component
   }
 
-  @Override
-  public Chart getChart() {
-    return getUiFacade().getResourceChart();
-  }
-
-  @Override
-  public Component getViewComponent() {
-    return getComponent();
+  init {
+    addTableResizeListeners(myTreeFacade.treeComponent, myTreeFacade.treeTable.scrollPane.viewport)
   }
 }
