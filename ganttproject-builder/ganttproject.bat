d38cbd605fd7e82a27ef3a57b6add0e10f5dcb17
Dmitry Barashev <dbarashev@ganttproject.biz>
2022-01-17 19:49:37 +0200

    Updated bat file for Windows

--- ganttproject-builder/ganttproject.bat
+++ ganttproject-builder/ganttproject.bat
@@ -6,12 +6,13 @@ SET "JAVA_COMMAND=%JAVA_HOME%\bin\java.exe"
 IF "%JAVA_HOME%"=="" SET JAVA_COMMAND=java
 
 SET LOCAL_CLASSPATH=%GP_HOME%\eclipsito.jar;%GP_HOME%
-SET JVM_ARGS=-Xmx512m  -ea -Dsun.java2d.d3d=false
-SET "ECLIPSITO_ARGS=--verbosity 1  --version-dirs plugins;~/.ganttproject.d/updates --app net.sourceforge.ganttproject.GanttProject"
+SET JVM_ARGS=-Xmx1024m  -ea -Dsun.java2d.d3d=false
+SET "ECLIPSITO_ARGS=--verbosity 4  --version-dirs plugins;~/.ganttproject.d/updates --app net.sourceforge.ganttproject.GanttProject"
+SET "JAVA_EXPORTS=--add-exports javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx=ALL-UNNAMED --add-exports javafx.controls/com.sun.javafx.scene.control=ALL-UNNAMED --add-exports javafx.controls/com.sun.javafx.scene.control.skin=ALL-UNNAMED --add-exports javafx.controls/com.sun.javafx.scene.control.skin.resources=ALL-UNNAMED --add-exports javafx.controls/com.sun.javafx.scene.control.inputmap=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.scene.traversal=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.scene=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.util=ALL-UNNAMED --add-opens java.desktop/sun.swing=ALL-UNNAMED"
 
 SET BOOT_CLASS=com.bardsoftware.eclipsito.Launch
 
-"%JAVA_COMMAND%" -classpath "%CLASSPATH%;%LOCAL_CLASSPATH%" %JVM_ARGS% %BOOT_CLASS% %ECLIPSITO_ARGS% -log true  %1 %2 %3 %4 %5 %6
+"%JAVA_COMMAND%" -classpath "%CLASSPATH%;%LOCAL_CLASSPATH%" %JVM_ARGS% %JAVA_EXPORTS% %BOOT_CLASS% %ECLIPSITO_ARGS% -log true  %1 %2 %3 %4 %5 %6
 if %ERRORLEVEL% EQU 9009 goto ERROR_NO_JAVA
 goto END
 
