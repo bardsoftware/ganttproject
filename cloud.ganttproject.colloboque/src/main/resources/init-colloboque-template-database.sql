13e1ff6f07c2fa401946856b6bcb73ada61c3c1e
Dmitry Barashev <dmitry@barashev.net>
2022-04-21 21:37:11 +0300

    Formatting, comments

--- cloud.ganttproject.colloboque/src/main/resources/init-colloboque-template-database.sql
+++ cloud.ganttproject.colloboque/src/main/resources/init-colloboque-template-database.sql
@@ -12,7 +12,7 @@ CREATE OR REPLACE FUNCTION clone_schema(
   RETURNS void AS
 $BODY$
 
---  This function will clone all sequences, tables, data, views & functions from any existing schema to a new one
+--  This function will clone all sequences, tables, triggers, data, views & functions from any existing schema to a new one
 -- SAMPLE CALL:
 -- SELECT clone_schema('public', 'new_schema', TRUE);
 
@@ -61,184 +61,189 @@ BEGIN
     WHERE nspname = quote_ident(source_schema);
     IF NOT FOUND
         THEN
-        RAISE NOTICE 'source schema % does not exist!', source_schema;
-        RETURN ;
+        RAISE EXCEPTION 'source schema % does not exist!', source_schema;
     END IF;
 
     EXECUTE 'CREATE SCHEMA ' || quote_ident(dest_schema) ;
 
--- Create sequences
--- TODO: Find a way to make this sequence's owner is the correct table.
-FOR object IN
-SELECT sequence_name::text
-FROM information_schema.sequences
-WHERE sequence_schema = quote_ident(source_schema)
+    -- Create sequences
+    -- TODO: Find a way to make this sequence's owner is the correct table.
+    FOR object IN
+    SELECT sequence_name::text
+    FROM information_schema.sequences
+    WHERE sequence_schema = quote_ident(source_schema)
     LOOP
-    EXECUTE 'CREATE SEQUENCE ' || quote_ident(dest_schema) || '.' || quote_ident(object);
-srctbl := quote_ident(source_schema) || '.' || quote_ident(object);
+        EXECUTE 'CREATE SEQUENCE ' || quote_ident(dest_schema) || '.' || quote_ident(object);
+        srctbl := quote_ident(source_schema) || '.' || quote_ident(object);
 
-EXECUTE 'SELECT last_value, max_value, start_value, increment_by, min_value, cache_value, log_cnt, is_cycled, is_called
-              FROM ' || quote_ident(source_schema) || '.' || quote_ident(object) || ';'
-    INTO sq_last_value, sq_max_value, sq_start_value, sq_increment_by, sq_min_value, sq_cache_value, sq_log_cnt, sq_is_cycled, sq_is_called ;
+        EXECUTE 'SELECT last_value, max_value, start_value, increment_by, min_value, cache_value, log_cnt, is_cycled, is_called
+                  FROM ' || quote_ident(source_schema) || '.' || quote_ident(object) || ';'
+        INTO sq_last_value, sq_max_value, sq_start_value, sq_increment_by, sq_min_value, sq_cache_value, sq_log_cnt, sq_is_cycled, sq_is_called ;
 
-IF sq_is_cycled
-      THEN
-        sq_cycled := 'CYCLE';
-ELSE
-        sq_cycled := 'NO CYCLE';
-END IF;
+        IF sq_is_cycled THEN
+            sq_cycled := 'CYCLE';
+        ELSE
+            sq_cycled := 'NO CYCLE';
+        END IF;
 
-EXECUTE 'ALTER SEQUENCE '   || quote_ident(dest_schema) || '.' || quote_ident(object)
-    || ' INCREMENT BY ' || sq_increment_by
-    || ' MINVALUE '     || sq_min_value
-    || ' MAXVALUE '     || sq_max_value
-    || ' START WITH '   || sq_start_value
-    || ' RESTART '      || sq_min_value
-    || ' CACHE '        || sq_cache_value
-    || sq_cycled || ' ;' ;
+        EXECUTE 'ALTER SEQUENCE '   || quote_ident(dest_schema) || '.' || quote_ident(object)
+            || ' INCREMENT BY ' || sq_increment_by
+            || ' MINVALUE '     || sq_min_value
+            || ' MAXVALUE '     || sq_max_value
+            || ' START WITH '   || sq_start_value
+            || ' RESTART '      || sq_min_value
+            || ' CACHE '        || sq_cache_value
+            || sq_cycled || ' ;' ;
 
-buffer := quote_ident(dest_schema) || '.' || quote_ident(object);
-    IF include_recs
-        THEN
+        buffer := quote_ident(dest_schema) || '.' || quote_ident(object);
+        IF include_recs THEN
             EXECUTE 'SELECT setval( ''' || buffer || ''', ' || sq_last_value || ', ' || sq_is_called || ');' ;
-ELSE
+        ELSE
             EXECUTE 'SELECT setval( ''' || buffer || ''', ' || sq_start_value || ', ' || sq_is_called || ');' ;
-END IF;
+        END IF;
+    END LOOP;
 
-END LOOP;
-
--- Create tables
-FOR object IN
-SELECT TABLE_NAME::text
-FROM information_schema.tables
-WHERE table_schema = quote_ident(source_schema)
-  AND table_type = 'BASE TABLE'
-
-    LOOP
-    buffer := dest_schema || '.' || quote_ident(object);
-EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE ' || quote_ident(source_schema) || '.' || quote_ident(object)
-    || ' INCLUDING ALL)';
-
-IF include_recs
-      THEN
-      -- Insert records from source table
-      EXECUTE 'INSERT INTO ' || buffer || ' SELECT * FROM ' || quote_ident(source_schema) || '.' || quote_ident(object) || ';';
-END IF;
-
-FOR column_, default_ IN
-SELECT column_name::text,
-    REPLACE(column_default::text, source_schema, dest_schema)
-FROM information_schema.COLUMNS
-WHERE table_schema = dest_schema
-  AND TABLE_NAME = object
-  AND column_default LIKE 'nextval(%' || quote_ident(source_schema) || '%::regclass)'
-    LOOP
-      EXECUTE 'ALTER TABLE ' || buffer || ' ALTER COLUMN ' || column_ || ' SET DEFAULT ' || default_;
-END LOOP;
-
-END LOOP;
-
---  add FK constraint
-FOR qry IN
-SELECT 'ALTER TABLE ' || quote_ident(dest_schema) || '.' || quote_ident(rn.relname)
-           || ' ADD CONSTRAINT ' || quote_ident(ct.conname) || ' ' || pg_get_constraintdef(ct.oid) || ';'
-FROM pg_constraint ct
-         JOIN pg_class rn ON rn.oid = ct.conrelid
-WHERE connamespace = src_oid
-  AND rn.relkind = 'r'
-  AND ct.contype = 'f'
+    -- Create tables
+    FOR object IN
+    SELECT TABLE_NAME::text
+    FROM information_schema.tables
+    WHERE table_schema = quote_ident(source_schema)
+      AND table_type = 'BASE TABLE'
 
     LOOP
-      EXECUTE qry;
+        buffer := dest_schema || '.' || quote_ident(object);
+        EXECUTE 'CREATE TABLE ' || buffer || ' (LIKE ' || quote_ident(source_schema) || '.' || quote_ident(object) || ' INCLUDING ALL)';
 
-END LOOP;
+        IF include_recs THEN
+            -- Insert records from source table
+            EXECUTE 'INSERT INTO ' || buffer || ' SELECT * FROM ' || quote_ident(source_schema) || '.' || quote_ident(object) || ';';
+        END IF;
 
+        FOR column_, default_ IN
+        SELECT column_name::text,
+            REPLACE(column_default::text, source_schema, dest_schema)
+        FROM information_schema.COLUMNS
+        WHERE table_schema = dest_schema
+          AND TABLE_NAME = object
+          AND column_default LIKE 'nextval(%' || quote_ident(source_schema) || '%::regclass)'
+        LOOP
+            EXECUTE 'ALTER TABLE ' || buffer || ' ALTER COLUMN ' || column_ || ' SET DEFAULT ' || default_;
+        END LOOP;
+    END LOOP;
 
--- Create views
-FOR object IN
-SELECT table_name::text,
-    view_definition
-FROM information_schema.views
-WHERE table_schema = quote_ident(source_schema)
+    --  add FK constraint
+    FOR qry IN
+    SELECT 'ALTER TABLE ' || quote_ident(dest_schema) || '.' || quote_ident(rn.relname) || ' ADD CONSTRAINT ' || quote_ident(ct.conname) || ' ' || pg_get_constraintdef(ct.oid) || ';'
+    FROM pg_constraint ct
+             JOIN pg_class rn ON rn.oid = ct.conrelid
+    WHERE connamespace = src_oid
+      AND rn.relkind = 'r'
+      AND ct.contype = 'f'
 
     LOOP
-    buffer := dest_schema || '.' || quote_ident(object);
-SELECT view_definition INTO v_def
-FROM information_schema.views
-WHERE table_schema = quote_ident(source_schema)
-  AND table_name = quote_ident(object);
+        EXECUTE qry;
+    END LOOP;
 
-EXECUTE 'CREATE OR REPLACE VIEW ' || buffer || ' AS ' || v_def || ';' ;
 
-END LOOP;
-
--- Create functions
-FOR func_oid IN
-SELECT oid
-FROM pg_proc
-WHERE pronamespace = src_oid
+    -- Create views
+    FOR object IN
+    SELECT table_name::text,
+        view_definition
+    FROM information_schema.views
+    WHERE table_schema = quote_ident(source_schema)
 
     LOOP
-SELECT pg_get_functiondef(func_oid) INTO qry;
-SELECT replace(qry, source_schema, dest_schema) INTO dest_qry;
-EXECUTE dest_qry;
+        buffer := dest_schema || '.' || quote_ident(object);
+        SELECT view_definition INTO v_def
+        FROM information_schema.views
+        WHERE table_schema = quote_ident(source_schema)
+          AND table_name = quote_ident(object);
 
-END LOOP;
+        EXECUTE 'CREATE OR REPLACE VIEW ' || buffer || ' AS ' || v_def || ';' ;
 
--- Create triggers
--- Source: https://dev.to/renatosuero/clone-schema-in-postgresql-4fpc
-FOR trigger_name_, trigger_timing_, trigger_events_, trigger_orientation_, trigger_action_ IN
-SELECT trigger_name::text, action_timing::text, string_agg(event_manipulation::text, ' OR '), action_orientation::text, action_statement::text FROM information_schema.TRIGGERS
-WHERE event_object_schema=source_schema and event_object_table=object
-GROUP BY trigger_name, action_timing, action_orientation, action_statement
+    END LOOP;
+
+    -- Create functions
+    FOR func_oid IN
+    SELECT oid
+    FROM pg_proc
+    WHERE pronamespace = src_oid
+
     LOOP
-		EXECUTE 'CREATE TRIGGER ' || trigger_name_ || ' ' || trigger_timing_ || ' ' || trigger_events_ || ' ON ' || buffer || ' FOR EACH ' || trigger_orientation_ || ' ' || trigger_action_;
-END LOOP;
+        SELECT pg_get_functiondef(func_oid) INTO qry;
+        SELECT replace(qry, source_schema, dest_schema) INTO dest_qry;
+        EXECUTE dest_qry;
+    END LOOP;
+
+    -- Create triggers
+    -- Source: https://dev.to/renatosuero/clone-schema-in-postgresql-4fpc
+    FOR trigger_name_, trigger_timing_, trigger_events_, trigger_orientation_, trigger_action_ IN
+    SELECT trigger_name::text, action_timing::text, string_agg(event_manipulation::text, ' OR '), action_orientation::text, action_statement::text FROM information_schema.TRIGGERS
+    WHERE event_object_schema=source_schema and event_object_table=object
+    GROUP BY trigger_name, action_timing, action_orientation, action_statement
+    LOOP
+       EXECUTE 'CREATE TRIGGER ' || trigger_name_ || ' ' || trigger_timing_ || ' ' || trigger_events_ || ' ON ' || buffer || ' FOR EACH ' || trigger_orientation_ || ' ' || trigger_action_;
+    END LOOP;
 
   RETURN;
-
 END;
 
 $BODY$
-LANGUAGE plpgsql VOLATILE
-  COST 100;
-ALTER FUNCTION clone_schema(text, text, boolean)
-    OWNER TO postgres;
+LANGUAGE plpgsql VOLATILE COST 100;
+
+ALTER FUNCTION clone_schema(text, text, boolean) OWNER TO postgres;
 
 ----------------------------------------------------------------------------------------------------------------
-
+-- This template schema is cloned for every "branch" of the project.
 CREATE SCHEMA project_template;
-set search_path TO project_template;
+SET search_path TO project_template;
 
-CREATE TABLE project_template.TaskName(uid TEXT PRIMARY KEY, num INT NOT NULL, name TEXT NOT NULL DEFAULT '');
-CREATE TABLE project_template.TaskDates(
-                                           uid TEXT PRIMARY KEY REFERENCES project_template.TaskName,
-                                           start_date DATE NOT NULL,
-                                           duration_days INT NOT NULL
+-- Basic task data
+CREATE TABLE TaskName(
+    uid TEXT PRIMARY KEY,
+    num INT NOT NULL,
+    name TEXT NOT NULL DEFAULT ''
 );
-CREATE TABLE project_template.TaskIntProperties(
-                                                   uid TEXT REFERENCES project_template.TaskName,
-                                                   prop_name TEXT,
-                                                   prop_value INT
+-- Task start date and duration shall be changed as a whole
+CREATE TABLE TaskDates(
+    uid           TEXT PRIMARY KEY REFERENCES TaskName,
+    start_date    DATE NOT NULL,
+    duration_days INT NOT NULL
 );
 
-CREATE VIEW project_template.Task AS
+-- Other task properties can be changed independently, so they are stored in rows, one row corresponds to one
+-- instance of the task property value
+
+CREATE TYPE IntPropertyName AS ENUM ('completion', 'priority');
+
+-- Integer valued properties
+CREATE TABLE TaskIntProperties(
+    uid        TEXT REFERENCES TaskName,
+    prop_name  IntPropertyName,
+    prop_value INT
+);
+
+-- Updatable view which collects all task properties in a single row. Inserts and updates are processed
+-- with INSTEAD OF triggers.
+CREATE VIEW Task AS
 SELECT uid, num, name, start_date, duration_days AS duration,
        MAX(prop_value) FILTER(WHERE prop_name = 'completion') AS completion
-from project_template.TaskName JOIN project_template.TaskDates USING(uid)
-                               LEFT JOIN project_template.TaskIntProperties USING(uid)
+from TaskName JOIN TaskDates USING(uid)
+         LEFT JOIN TaskIntProperties USING(uid)
 GROUP BY uid, TaskDates.uid;
 
-CREATE OR REPLACE FUNCTION project_template.update_task_row() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION update_task_row() RETURNS TRIGGER AS $$
 BEGIN
-INSERT INTO project_template.TaskName(uid, num, name) SELECT NEW.uid, NEW.num, NEW.name
-    ON CONFLICT(uid) DO UPDATE SET num=NEW.num, name=NEW.name;
+INSERT INTO TaskName(uid, num, name)
+SELECT NEW.uid, NEW.num, NEW.name
+ON CONFLICT(uid) DO UPDATE SET num=NEW.num, name=NEW.name;
 
-INSERT INTO project_template.TaskDates(uid, start_date, duration_days)
+INSERT INTO TaskDates(uid, start_date, duration_days)
 SELECT NEW.uid, NEW.start_date, NEW.duration
-    ON CONFLICT(uid) DO UPDATE SET start_date=NEW.start_date, duration_days=NEW.duration;
+ON CONFLICT(uid) DO UPDATE SET start_date=NEW.start_date, duration_days=NEW.duration;
 RETURN NEW;
-end;
+END;
+
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER update_task INSTEAD OF UPDATE ON Task FOR EACH ROW EXECUTE FUNCTION update_task_row();
