1e3218d79772919010b13b8cf157c0a6618f318f
npostnikova <postnikovaanastasiaa@gmail.com>
2022-04-22 21:34:02 +0300

    Add WebSocket skeleton

--- cloud.ganttproject.colloboque/src/main/kotlin/cloud/ganttproject/colloboque/DevServer.kt
+++ cloud.ganttproject.colloboque/src/main/kotlin/cloud/ganttproject/colloboque/DevServer.kt
@@ -24,17 +24,21 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
 import fi.iki.elonen.NanoHTTPD
 import fi.iki.elonen.NanoHTTPD.Response.Status
+import fi.iki.elonen.NanoWSD
 import kotlinx.coroutines.channels.Channel
+import net.sourceforge.ganttproject.GPLogger
 import org.slf4j.LoggerFactory
+import java.io.IOException
 
 fun main(args: Array<String>) = DevServerMain().main(args)
 
 class DevServerMain : CliktCommand() {
-  val port by option("--port").int().default(9000)
-  val pgHost by option("--pg-host").default("localhost")
-  val pgPort by option("--pg-port").int().default(5432)
-  val pgSuperUser by option("--pg-super-user").default("postgres")
-  val pgSuperAuth by option("--pg-super-auth").default("")
+  private val port by option("--port").int().default(9000)
+  private val wsPort by option("--ws-port").int().default(9001)
+  private val pgHost by option("--pg-host").default("localhost")
+  private val pgPort by option("--pg-port").int().default(5432)
+  private val pgSuperUser by option("--pg-super-user").default("postgres")
+  private val pgSuperAuth by option("--pg-super-auth").default("")
 
   override fun run() {
     LoggerFactory.getLogger("Startup").info("Starting dev Colloboque server on port $port")
@@ -44,6 +48,7 @@ class DevServerMain : CliktCommand() {
     val dataSourceFactory = PostgresDataSourceFactory(pgHost, pgPort, pgSuperUser, pgSuperAuth)
     val colloboqueServer = ColloboqueServer(dataSourceFactory::createDataSource, initInputChannel, updateInputChannel)
     ColloboqueHttpServer(port, colloboqueServer).start(NanoHTTPD.SOCKET_READ_TIMEOUT, false)
+    ColloboqueWebSocketServer(wsPort, colloboqueServer).start(NanoHTTPD.SOCKET_READ_TIMEOUT, false)
   }
 }
 
@@ -59,5 +64,37 @@ class ColloboqueHttpServer(port: Int, private val colloboqueServer: ColloboqueSe
       "/" -> newFixedLengthResponse("Hello")
       else -> newFixedLengthResponse(Status.NOT_FOUND, MIME_PLAINTEXT, "Not Found")
     }
-
 }
+
+class ColloboqueWebSocketServer(port: Int, private val colloboqueServer: ColloboqueServer) : NanoWSD("localhost", port) {
+  override fun openWebSocket(handshake: IHTTPSession?): WebSocket {
+    return WebSocketImpl(handshake)
+  }
+
+  private class WebSocketImpl(handshake: IHTTPSession?): WebSocket(handshake) {
+    override fun onOpen() {
+      LOG.debug("WebSocket opened")
+    }
+
+    override fun onClose(code: WebSocketFrame.CloseCode?, reason: String?, initiatedByRemote: Boolean) {
+      LOG.debug("WebSocket closed")
+    }
+
+    override fun onMessage(message: WebSocketFrame?) {
+      try {
+        LOG.debug("Message received ${message?.textPayload}")
+        send(message?.textPayload)
+      } catch (e: IOException) {
+        LOG.error("Failed to send response {}", e)
+      }
+    }
+
+    override fun onPong(pong: WebSocketFrame?) {}
+
+    override fun onException(exception: IOException?) {
+      LOG.error("WebSocket exception $exception")
+    }
+  }
+}
+
+private val LOG = GPLogger.create("ColloboqueWebServer")
