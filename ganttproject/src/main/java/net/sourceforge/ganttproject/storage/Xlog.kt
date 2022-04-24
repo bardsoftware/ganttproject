1345ceab5dee10ef84ce6a312bf6e6e8b3a37254
npostnikova <postnikovaanastasiaa@gmail.com>
2022-04-24 21:11:35 +0300

    Move xlog file

--- ganttproject/src/main/java/net/sourceforge/ganttproject/storage/Xlog.kt
+++ ganttproject/src/main/java/net/sourceforge/ganttproject/storage/Xlog.kt
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GanttProject.  If not, see <http://www.gnu.org/licenses/>.
 */
-package cloud.ganttproject.colloboque
+package net.sourceforge.ganttproject.storage
 
 /**
  * Data for the initialization of the Colloboque database.
