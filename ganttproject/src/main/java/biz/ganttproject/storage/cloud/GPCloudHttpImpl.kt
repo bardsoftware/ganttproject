12d6257d1398447369812e4a530da0207dd54d3e
edgarzhavoronkov <edzhavoronkov@gmail.com>
2022-09-14 13:46:38 +0300

    decode server response properly(it wouldn't work otherwise)

--- ganttproject/src/main/java/biz/ganttproject/storage/cloud/GPCloudHttpImpl.kt
+++ ganttproject/src/main/java/biz/ganttproject/storage/cloud/GPCloudHttpImpl.kt
@@ -359,7 +359,7 @@ class WebSocketClient {
   private fun fireCommitResponseReceived(payload: ObjectNode) {
     LOG.debug("Commit response received:\n {}", payload)
     try {
-      val serverResponse = Json.decodeFromString<ServerResponse.CommitResponse>(payload.toString())
+      val serverResponse = Json.decodeFromString(ServerResponse.serializer(), payload.toString()) as ServerResponse.CommitResponse
       xlogCommitResponseListeners.forEach { it(serverResponse) }
     } catch (e: Exception) {
       LOG.debug("Failed to parse ServerCommitResponse:\n {}", payload)
