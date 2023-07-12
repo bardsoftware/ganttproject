997295b915f46ab94ad0d1613a6ab65668f172fe
Dmitry Barashev <dbarashev@ganttproject.biz>
2023-07-12 23:42:19 +0300

    it looks like there is an invalid command line

--- build-bin/package-mac.sh
+++ build-bin/package-mac.sh
@@ -34,11 +34,11 @@ jpackage --type app-image \
     --name "GanttProject" \
     --input "${INPUT}" \
     --dest "${OUTPUT}" \
-    --java-options "-Dapple.laf.useScreenMenuBar=true
+    --java-options '-Dapple.laf.useScreenMenuBar=true
                     -Dcom.apple.macos.useScreenMenuBar=true 
                     -Dcom.apple.mrj.application.apple.menu.about.name=GanttProject
                     -Dsun.java2d.metal=true
-                    -Xdock:name=$APP_NAME"' -Xdock:icon=$APPDIR/ganttproject.icns'" \
+                    -Xdock:name=GanttProject -Xdock:icon=$APPDIR/ganttproject.icns \
                     -Xmx2048m \
                     -Dfile.encoding=UTF-8
                     --add-exports javafx.controls/com.sun.javafx.scene.control.behavior=ALL-UNNAMED
@@ -54,11 +54,11 @@ jpackage --type app-image \
                     --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED
                     --add-exports javafx.graphics/com.sun.javafx.util=ALL-UNNAMED
                     --add-opens java.desktop/sun.swing=ALL-UNNAMED \
-                    -classpath "'$APPDIR:$APPDIR/eclipsito.jar:$APPDIR/lib/slf4j-api-2.0.4.jar:$APPDIR/lib/slf4j-jdk14-2.0.4.jar'"\
-                    -Duser.dir="'$APPDIR'" -DversionDirs=plugins:~/.ganttproject.d/updates \
+                    -classpath $APPDIR:$APPDIR/eclipsito.jar:$APPDIR/lib/slf4j-api-2.0.4.jar:$APPDIR/lib/slf4j-jdk14-2.0.4.jar \
+                    -Duser.dir=$APPDIR -DversionDirs=plugins:~/.ganttproject.d/updates \
                     -Dapp=net.sourceforge.ganttproject.GanttProject
                     -Dorg.jooq.no-logo=true
-                    -Dgpcloud=prod" \
+                    -Dgpcloud=prod' \
     --arguments '--verbosity 4' \
     --arguments '--version-dirs plugins:~/.ganttproject.d/updates' \
     --arguments '--app net.sourceforge.ganttproject.GanttProject' \
