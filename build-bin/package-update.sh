7db6c0eab5f1375edd221b2a7e0941e51811121f
Dmitry Barashev <dbarashev@ganttproject.biz>
2024-02-29 14:43:58 +0400

    bugfix

--- build-bin/package-update.sh
+++ build-bin/package-update.sh
@@ -2,7 +2,9 @@
 set -o errexit
 
 REPO_ROOT=$(pwd)
+echo "Work dir: $REPO_ROOT"
 mkdir -p build/distributions
+ls -l $REPO_ROOT/build/distributions
 VERSION=$(cat ganttproject-builder/BUILD-HISTORY-MINOR | tail -n 1 | awk '{print $2}')
 [[ -z "$VERSION" ]] && exit 1
 echo "Building version $VERSION"
@@ -15,6 +17,5 @@ cd ganttproject-builder/dist-bin/plugins/
 mkdir update-$VERSION
 IFS=','
 for m in $MODULES; do cp -R base/$m update-$VERSION/ ; done
-zip -r $REPO_ROOT/build/distributions/update-$VERSION.zip update-$VERSION
-echo "The update was created in $REPO_ROOT/build/distributions"
-ls -l $REPO_ROOT/build/distributions
+zip -r "update-$VERSION.zip" "update-$VERSION"
+gsutil cp "update-$VERSION.zip" gs://dl.ganttproject.biz/updates/ && gsutil acl ch -u AllUsers:R "gs://dl.ganttproject.biz/updates/update-$VERSION.zip";
