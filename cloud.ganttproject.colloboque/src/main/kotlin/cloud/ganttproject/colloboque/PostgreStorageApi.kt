be1b3fbcc6996209d78635be91d33bd068f57297
veron <veronika@telamon.ru>
2024-04-20 21:44:45 +0200

    add comment

--- cloud.ganttproject.colloboque/src/main/kotlin/cloud/ganttproject/colloboque/PostgreStorageApi.kt
+++ cloud.ganttproject.colloboque/src/main/kotlin/cloud/ganttproject/colloboque/PostgreStorageApi.kt
@@ -147,14 +147,18 @@ class PostgreStorageApi(private val connectionFactory: PostgresConnectionFactory
       clientTransaction.forEach {
         it.colloboqueOperations.forEach {
           LOG.debug("... applying operation={}", it)
+          // Hangs here. When `serverDsl.commit().execute()` is moved
+          // before the `clientTransaction.forEach { ... }` block, the whole
+          // function executes successfully and returns true even if
+          // there should have been a conflict. Transaction isolation level
+          // setting doesn't affect this behaviour
           clientDsl.execute(generateSqlStatement(clientDsl, it))
         }
       }
       LOG.debug("... commiting server's transaction")
-      serverDsl.commit().execute()
+      if (serverDsl.commit().execute() != 0) return false
       LOG.debug("... commiting client's transaction")
-      clientDsl.commit().execute()
-      return true
+      return clientDsl.commit().execute() == 0
     } catch (e: Exception) {
       LOG.info("Failed to execute transactions in parallel! Reason: ${e.localizedMessage}")
     }
