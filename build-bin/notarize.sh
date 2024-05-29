8c9c4e8c50c5592cb790e6873830c370dd52d53f
Dmitry Barashev <dbarashev@ganttproject.biz>
2024-05-29 13:02:16 +0400

    test build+codesign

--- build-bin/notarize.sh
+++ build-bin/notarize.sh
@@ -4,16 +4,15 @@ VER=$1
 SIG=$2
 NOTARIZE_PASSWORD=$3
 COMMAND=$4
+KEYCHAIN=${5:-"~/Library/Keychains/login.keychain-db"}
 
 do_prepare() {
-#find build/GanttProject.app/ -type f -not -path *Contents/runtime/* -not -path */Contents/MacOS/GanttProject -not -path *libapplauncher.dylib -exec codesign --timestamp -f -s "$SIG" --prefix com.bardsoftware. --entitlements build-cfg/ganttproject.entitlements.xml --options runtime -v --keychain ~/Library/Keychains/login.keychain-db {} \;
-#find build/GanttProject.app/Contents/runtime -type f -not -path */legal/* -not -path */man/* -exec codesign --timestamp -f -s "$SIG" --prefix com.bardsoftware. --entitlements build-cfg/ganttproject.entitlements.xml --options runtime -v --keychain ~/Library/Keychains/login.keychain-db {} \;
-#codesign -f --timestamp --entitlements build-cfg/ganttproject.entitlements.xml -s "$SIG" --prefix com.bardsoftware. --options runtime -v build/GanttProject.app/Contents/runtime
-#codesign -f --timestamp --entitlements build-cfg/ganttproject.entitlements.xml -s "$SIG" --prefix com.bardsoftware. --options runtime -v build/GanttProject.app
-#codesign -vvv --deep --strict build/GanttProject.app
-#spctl -a -t exec -vv build/GanttProject.app
-
-security dump-keychain build.keychain
+find build/GanttProject.app/ -type f -not -path *Contents/runtime/* -not -path */Contents/MacOS/GanttProject -not -path *libapplauncher.dylib -exec codesign --timestamp -f -s "$SIG" --prefix com.bardsoftware. --entitlements build-cfg/ganttproject.entitlements.xml --options runtime -v --keychain "$KEYCHAIN" {} \;
+find build/GanttProject.app/Contents/runtime -type f -not -path */legal/* -not -path */man/* -exec codesign --timestamp -f -s "$SIG" --prefix com.bardsoftware. --entitlements build-cfg/ganttproject.entitlements.xml --options runtime -v --keychain "$KEYCHAIN" {} \;
+codesign -f --timestamp --entitlements build-cfg/ganttproject.entitlements.xml -s "$SIG" --prefix com.bardsoftware. --options runtime -v build/GanttProject.app/Contents/runtime
+codesign -f --timestamp --entitlements build-cfg/ganttproject.entitlements.xml -s "$SIG" --prefix com.bardsoftware. --options runtime -v build/GanttProject.app
+codesign -vvv --deep --strict build/GanttProject.app
+spctl -a -t exec -vv build/GanttProject.app
 }
 
 do_notarize() {
