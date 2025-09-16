25f04cef136aeec6e66551e4eb7d68ab8603a28d
Dmitry Barashev <dbarashev@ganttproject.biz>
2025-09-16 21:44:33 +0400

    Simple property editor test

---
+++ ganttproject/src/test/java/biz/ganttproject/app/PropertySheetTest.kt
@@ -0,0 +1,33 @@
+package biz.ganttproject.app
+
+import biz.ganttproject.core.option.ObservableString
+import javafx.scene.control.TextField
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.javafx.JavaFx
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.junit.jupiter.api.Test
+
+class PropertySheetTest {
+  private val coroutineScope = CoroutineScope(SupervisorJob())
+
+  @Test fun `basic string property`() {
+    coroutineScope.launch {
+      withContext(Dispatchers.JavaFx) {
+        val propertyPaneBuilder = PropertyPaneBuilder(DummyLocalizer, PropertyPane())
+        val textProperty = ObservableString("prop1", "Lorem Ipsum")
+        val editor = propertyPaneBuilder.createStringOptionEditor(textProperty)
+        (editor as? TextField)?.let {
+          it.text = "Hello"
+          assert(textProperty.value == "Hello")
+
+          textProperty.value = "Hello2"
+          assert(it.text == "Hello2")
+        }
+      }
+    }
+  }
+
+}
