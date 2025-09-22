c19bbaa37ba5aac4779e253ba809962a653e55c2
Dmitry Barashev <dbarashev@ganttproject.biz>
2025-09-22 10:52:28 +0400

    try/catch errors

--- ganttproject/src/test/java/biz/ganttproject/app/PropertySheetTest.kt
+++ ganttproject/src/test/java/biz/ganttproject/app/PropertySheetTest.kt
@@ -5,6 +5,7 @@ import javafx.scene.control.TextField
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.javafx.JavaFx
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -13,19 +14,24 @@ import org.junit.jupiter.api.Test
 class PropertySheetTest {
   private val coroutineScope = CoroutineScope(SupervisorJob())
 
-  fun `basic string property`() {
+  @Test fun `basic string property`() {
     coroutineScope.launch {
-      withContext(Dispatchers.JavaFx) {
-        val propertyPaneBuilder = PropertyPaneBuilder(DummyLocalizer, PropertyPane())
-        val textProperty = ObservableString("prop1", "Lorem Ipsum")
-        val editor = propertyPaneBuilder.createStringOptionEditor(textProperty)
-        (editor as? TextField)?.let {
-          it.text = "Hello"
-          assert(textProperty.value == "Hello")
+      try {
+        withContext(Dispatchers.JavaFx) {
+          val propertyPaneBuilder = PropertyPaneBuilder(DummyLocalizer, PropertyPane())
+          val textProperty = ObservableString("prop1", "Lorem Ipsum")
+          val editor = propertyPaneBuilder.createStringOptionEditor(textProperty)
+          (editor as? TextField)?.let {
+            it.text = "Hello"
+            assert(textProperty.value == "Hello")
 
-          textProperty.value = "Hello2"
-          assert(it.text == "Hello2")
+            textProperty.value = "Hello2"
+            assert(it.text == "Hello2")
+          }
         }
+      } catch (ex: Exception) {
+        ex.printStackTrace()
+        this.cancel()
       }
     }
   }
